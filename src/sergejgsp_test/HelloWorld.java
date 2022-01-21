70ec23cfea2a930d5e33909ca5d85ecc965ce94c
Sergej <sergej777@inbox.lv>
2022-01-21 14:46:14 +0200

    My first Commit

--- src/sergejgsp_test/HelloWorld.java
+++ src/sergejgsp_test/HelloWorld.java
@@ -1,6 +1,6 @@
-package sergejgsp_test;
+package student_sergejgsp_test;
 
 public class HelloWorld {
 }
     public static void main(String[] args)
-        System.out.println("Hello World!!!"
+        System.out.println("Hello World!!!
