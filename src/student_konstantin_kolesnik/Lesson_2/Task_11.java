1bb2b6b91065a2ca33eab9500628226146bd090c
CO5M1C <cosinous@gmail.com>
2022-01-22 14:24:04 +0200

    // Name Change (Lowercase) // Homework: Lesson_2 - Task_11

---
+++ src/student_konstantin_kolesnik/Lesson_2/Task_11.java
@@ -0,0 +1,38 @@
+package student_konstantin_kolesnik.Lesson_2;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        // Version 1
+        System.out.println("Test Data:");
+        System.out.println("74 + 36");
+        System.out.println("Expected Output:");
+        System.out.println(74 + 36);
+        System.out.println();
+
+        //Version 1.1
+        System.out.println("Test Data:");
+        System.out.println("74 + 36");
+        var a = 74;
+        var b = 36;
+        var result = a + b;
+        System.out.println("Expected Output:");
+        System.out.println(result);
+        System.out.println();
+
+        // Version 2
+        System.out.println("Test Data:");
+        System.out.println("50 / 3");
+        System.out.println("Expected Output:");
+        System.out.println(50 / 3);
+        System.out.println();
+
+        //Version 2.1
+        System.out.println("Test Data:");
+        System.out.println("50/3");
+        var c = 50;
+        var d = 3;
+        var result2 = c / d;
+        System.out.println("Expected Output:");
+        System.out.println(result2);
+    }
+}
