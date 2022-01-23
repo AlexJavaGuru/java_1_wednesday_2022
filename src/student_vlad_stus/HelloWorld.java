78fd0361be6c3ce11c0c163f76476c5ba5089d4d
Prolog952 <prolog952@gmail.com>
2022-01-23 18:48:31 +0200

    VL@D: My First Commit

--- src/student_vlad_stus/HelloWorld.java
+++ src/student_vlad_stus/HelloWorld.java
@@ -3,6 +3,9 @@ package student_vlad_stus;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World...");
+        int k =1;
+        System.out.println(k);
+
     }
 
 }
