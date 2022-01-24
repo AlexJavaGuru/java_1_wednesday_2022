388a054fb1417d3bb8841a301586ecaf3a5efbb6
il4real <iljajantikov@inbox.lv>
2022-01-24 09:21:24 +0200

    Ilja Jantikovs commit3 test lessons together

--- src/student_ilja_jantikovs/test_lesson.java
+++
@@ -1,8 +0,0 @@
-package student_ilja_jantikovs;
-
-public class test_lesson {
-    public static void main(String[] args) {
-        System.out.println(74+36);
-        System.out.println(50/16);
-    }
-}
