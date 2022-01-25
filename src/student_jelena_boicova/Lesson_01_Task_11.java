c20e66cd8e0d44a49bec8a4c921c963d409d963a
Jelena01 <jelenaboicova01@gmail.com>
2022-01-25 16:26:59 +0200

    Jelena: Sum of Two Numbers

---
+++ src/student_jelena_boicova/Lesson_01_Task_11.java
@@ -0,0 +1,14 @@
+package student_jelena_boicova;
+
+public class Lesson_01_Task_11 {
+
+    public static void main(String[] args) {
+        int num_1=74,num_2=36,sum;
+        //declare variables
+        sum=num_1+num_2;
+        //find total of two numbers(num_1,num_2)
+
+        System.out.println("Sum of given numbers: "+sum);
+        //display sum of numbers
+    }
+}
