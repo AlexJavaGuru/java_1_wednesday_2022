9b284a24068e55610e745d466d71ed9e8d08857f
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-01-26 00:26:01 +0200

    Fixing

--- src/student_aleksandr_zuk/task1sum.java
+++ src/student_aleksandr_zuk/task1sum.java
@@ -2,10 +2,11 @@ package student_aleksandr_zuk;
 
 
 public class task1sum {
-        public static void main(String[] args) {
-            System.out.println(74 + 36);
-            System.out.println(50 / 3);
-        }
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+        System.out.println(50 / 3);
+    }
+}
 
 
 
