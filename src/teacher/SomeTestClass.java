bc5d9e7dcc2b3b81fd0174f5ad25502c8c991955
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-01-26 19:42:20 +0200

    Lesson1: Fixing code review comments

--- src/teacher/SomeTestClass.java
+++ src/teacher/SomeTestClass.java
@@ -4,5 +4,6 @@ public class SomeTestClass {
 
     public static void main(String[] args) {
         System.out.println("Hello from test class");
+        System.out.println("What exactly?");
     }
 }
