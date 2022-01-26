ec04ef482881d2e76625adad46cd7766bbed321c
NightCatX <kate.soldatova93@gmail.com>
2022-01-26 19:08:04 +0200

    NightCatX second commit

--- src/student_jekaterina_soldatova/lesson01ex10.java
+++ src/student_jekaterina_soldatova/lesson01ex10.java
@@ -17,7 +17,7 @@ Write a Java program to print the sum of two numbers.
 
 package student_jekaterina_soldatova;
 
-public class lesson01_10 {
+public class lesson01ex10 {
     public static void main(String[] args) {
         System.out.println(74+36);
         System.out.println(50/3);
