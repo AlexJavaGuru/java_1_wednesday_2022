3de8234805a704d56c30c76b9bad27ebca94e69b
JaKo <jako2577@gmail.com>
2022-01-27 19:20:07 +0200

    Level 5

---
+++ src/student_aleksandrs_jakovenko/Lesson2/Level5/SizeMatters.java
@@ -0,0 +1,8 @@
+package student_aleksandrs_jakovenko.Lesson2.Level5;
+
+public class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
---
+++ src/student_aleksandrs_jakovenko/Lesson2/Level5/Task15.java
@@ -0,0 +1,8 @@
+package student_aleksandrs_jakovenko.Lesson2.Level5;
+
+public class Task15 {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("World!");
+    }
+}
