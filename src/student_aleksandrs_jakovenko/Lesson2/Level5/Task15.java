5f6897468163cccc068a7f8aecb643efb7649344
JaKo <jako2577@gmail.com>
2022-01-27 19:25:07 +0200

    Level 6

--- src/student_aleksandrs_jakovenko/Lesson2/Level5/Task15.java
+++ src/student_aleksandrs_jakovenko/Lesson2/Level5/Task15.java
@@ -2,7 +2,7 @@ package student_aleksandrs_jakovenko.Lesson2.Level5;
 
 public class Task15 {
     public static void main(String[] args) {
-        System.out.print("Hello ");
-        System.out.print("World!");
+        System.out.print("Hello");
+        System.out.print(" World!");
     }
 }
