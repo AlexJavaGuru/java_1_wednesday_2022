8773bea53813602a605a04b9fda7070abf86a1dc
Sergej <sergej777@inbox.lv>
2022-01-27 16:02:30 +0200

    Calculation

---
+++ src/student_sergejgsp/CalculateFourNumbers.java
@@ -0,0 +1,11 @@
+package student_sergejgsp;
+
+public class CalculateFourNumbers {
+    public static void main(String[] args) {
+        int x = 72;
+        int y = 36;
+        int z = 84;
+        int a = 25;
+        System.out.println(x / y * z + a);
+
+    }}
