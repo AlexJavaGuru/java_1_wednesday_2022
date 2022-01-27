d80c91adf49580aadff16d0cbc1a305558d064fd
JaKo <jako2577@gmail.com>
2022-01-27 19:05:44 +0200

    Task11

---
+++ src/student_aleksandrs_jakovenko/Lesson2/Level3/Task11.java
@@ -0,0 +1,19 @@
+package student_aleksandrs_jakovenko.Lesson2.Level3;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number:");
+        int first = scanner.nextInt();
+        System.out.println("Enter second number:");
+        int second = scanner.nextInt();
+        System.out.println("Enter third number:");
+        int third = scanner.nextInt();
+
+        double average = (first + second + third)/3.0;
+        System.out.println("Average = " + average);
+    }
+}
