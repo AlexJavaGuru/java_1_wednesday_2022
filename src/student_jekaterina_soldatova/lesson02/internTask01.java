e89492c8d6a81b13695e2270a696efcf92c49fee
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 21:24:41 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/internTask01.java
+++ src/student_jekaterina_soldatova/lesson02/internTask01.java
@@ -1,11 +1,16 @@
-/* Написать консольную программу, которая запрашивает у пользователя два вещественных числа
-        (числа с запятой) и выводит на консоль результаты: сложения, вычитания, умножения и деления.
-        Вещественные числа можно запрашивать с консоли используя обьект Scanner
-        следующим образом:
+/*
+        Написать консольную программу, которая запрашивает у пользователя два целых числа и
+        выводит на консоль результаты: сложения, вычитания, умножения и деления.
 
+        1. Создать класс с нужным именем;
+        2. Создать описать main метод;
+        3. Воспользуйтесь специальным классом Scanner и его методом nextInt() для получения ввода с клавиатуры
         Scanner scanner = new Scanner(System.in);
-        double firstDoubleNumber = scanner.nextDouble();
- */
+        int firstNumber = scanner.nextInt();
+        4. Запишите полученные результаты в переменные;
+        5. При помощи System.out.println() выведите на консоль полученные результаты сложения, вычитания, умножения и деления.
+        (каждый результат на новой строке отдельно).
+*/
 
 package student_jekaterina_soldatova.lesson02;
 
@@ -14,14 +19,14 @@ import java.util.Scanner;
 public class internTask01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter first integer number");
-        int firstNumber = scanner.nextInt();
-        System.out.println("Please enter second integer number");
-        int secondNumber = scanner.nextInt();
+        System.out.println("Please enter first real number");
+        double firstDoubleNumber = scanner.nextDouble();
+        System.out.println("Please enter second real number");
+        double secondDoubleNumber = scanner.nextDouble();
 
-        System.out.println("Sum of your numbers is " + (firstNumber + secondNumber) );
-        System.out.println("Difference of your numbers is " + (firstNumber - secondNumber) );
-        System.out.println("Product of your numbers is " + (firstNumber * secondNumber) );
-        System.out.println("Quotient of your numbers is " + ((double)firstNumber / secondNumber) );
+        System.out.println("Sum of your numbers is " + (firstDoubleNumber + secondDoubleNumber) );
+        System.out.println("Difference of your numbers is " + (firstDoubleNumber - secondDoubleNumber) );
+        System.out.println("Product of your numbers is " + (firstDoubleNumber * secondDoubleNumber) );
+        System.out.println("Quotient of your numbers is " + (firstDoubleNumber / secondDoubleNumber) );
     }
 }
--- src/student_jekaterina_soldatova/lesson02/internTask02.java
+++ src/student_jekaterina_soldatova/lesson02/internTask02.java
@@ -1,16 +1,11 @@
-/*
-        Написать консольную программу, которая запрашивает у пользователя два целых числа и
-        выводит на консоль результаты: сложения, вычитания, умножения и деления.
+/* Написать консольную программу, которая запрашивает у пользователя два вещественных числа
+        (числа с запятой) и выводит на консоль результаты: сложения, вычитания, умножения и деления.
+        Вещественные числа можно запрашивать с консоли используя обьект Scanner
+        следующим образом:
 
-        1. Создать класс с нужным именем;
-        2. Создать описать main метод;
-        3. Воспользуйтесь специальным классом Scanner и его методом nextInt() для получения ввода с клавиатуры
         Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
-        4. Запишите полученные результаты в переменные;
-        5. При помощи System.out.println() выведите на консоль полученные результаты сложения, вычитания, умножения и деления.
-        (каждый результат на новой строке отдельно).
-*/
+        double firstDoubleNumber = scanner.nextDouble();
+ */
 
 package student_jekaterina_soldatova.lesson02;
 
@@ -19,14 +14,14 @@ import java.util.Scanner;
 public class internTask02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter first real number");
-        double firstDoubleNumber = scanner.nextDouble();
-        System.out.println("Please enter second real number");
-        double secondDoubleNumber = scanner.nextDouble();
+        System.out.println("Please enter first integer number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please enter second integer number");
+        int secondNumber = scanner.nextInt();
 
-        System.out.println("Sum of your numbers is " + (firstDoubleNumber + secondDoubleNumber) );
-        System.out.println("Difference of your numbers is " + (firstDoubleNumber - secondDoubleNumber) );
-        System.out.println("Product of your numbers is " + (firstDoubleNumber * secondDoubleNumber) );
-        System.out.println("Quotient of your numbers is " + (firstDoubleNumber / secondDoubleNumber) );
+        System.out.println("Sum of your numbers is " + (firstNumber + secondNumber) );
+        System.out.println("Difference of your numbers is " + (firstNumber - secondNumber) );
+        System.out.println("Product of your numbers is " + (firstNumber * secondNumber) );
+        System.out.println("Quotient of your numbers is " + ((double)firstNumber / secondNumber) );
     }
 }
