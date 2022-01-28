69b829666074f48d8d86c55ee78bdb8f2a43865e
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 21:51:34 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/internTask03.java
+++ src/student_jekaterina_soldatova/lesson02/internTask03.java
@@ -1,9 +1,8 @@
 package student_jekaterina_soldatova.lesson02;
 
-public class internTask03 {
+class internTask03 {
     public static void main(String[] args) {
         Book myObj = new Book();
-        //String book = myObj.x;
         System.out.println(myObj.x);
     }
 }
