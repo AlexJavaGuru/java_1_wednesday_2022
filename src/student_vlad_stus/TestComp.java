d76134957a7f543b25b25529c9d27502f4eb9b2c
Prolog952 <prolog952@gmail.com>
2022-01-28 22:32:00 +0200

    VL@D: LessonCode_test1

---
+++ src/student_vlad_stus/TestComp.java
@@ -0,0 +1,13 @@
+package student_vlad_stus;
+
+public class TestComp {
+
+    public static void main(String[] args) {
+
+        int age = 18;
+        float rise = 20.4f;
+        boolean hungry = false;//false
+        char symbol = 'A'; // нельзя использовать два символа сразу ! всегда зключается в одинарные ковычки
+        System.out.println();
+    }
+}
