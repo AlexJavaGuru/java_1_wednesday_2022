1b9c011781b4a5e2ca2337be003ed66918e07eae
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 22:53:45 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/BankAccount.java
+++ src/student_jekaterina_soldatova/lesson02/BankAccount.java
@@ -1,7 +1,21 @@
+/*
+        Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+
+class BankAccount {
+
+    public static void main(String[] args) {
+
+    }
+
+ */
+
+//please run internTask05
+
 package student_jekaterina_soldatova.lesson02;
 
 public class BankAccount {
     long min = 100000000000L;
     long max = 999999999999L;
-    long x = (long) (Math.random() * (max-min) + min);   //how clean is it? meh? how can it be improved?
+    long x = (long) (Math.random() * (max-min) + min);   //how clean is it? meh? can it be improved?
 }
