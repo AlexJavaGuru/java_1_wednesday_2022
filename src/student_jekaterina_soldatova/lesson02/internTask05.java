8dc7b0570821850a2a7fe557af830bbfee67f788
NightCatX <kate.soldatova93@gmail.com>
2022-01-29 01:27:48 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/internTask05.java
+++ src/student_jekaterina_soldatova/lesson02/internTask05.java
@@ -15,6 +15,7 @@ package student_jekaterina_soldatova.lesson02;
 public class internTask05 {
     public static void main(String[] args) {
         BankAccount accountGenerator = new BankAccount();
+        System.out.println("Bank account number template: 2 letters + 8-20 digits");
         System.out.println("Random bank account number: " + accountGenerator.randomLetters + accountGenerator.generateRandomDigits());
     }
 }
