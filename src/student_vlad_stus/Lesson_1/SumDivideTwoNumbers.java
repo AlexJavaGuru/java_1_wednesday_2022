0b228908a247942df634a63a2bcc79fb3c0181ca
Prolog952 <prolog952@gmail.com>
2022-01-28 20:23:16 +0200

    VL@D: Lesson1_SumDivideTwoNumbers

--- src/student_vlad_stus/Lesson_1/SumDivideTwoNumbers.java
+++ src/student_vlad_stus/Lesson_1/SumDivideTwoNumbers.java
@@ -1,6 +1,6 @@
 package student_vlad_stus.Lesson_1;
 
-public class Numbers {
+public class SumDivideTwoNumbers {
     public static void main(String[] args) {
         System.out.println(74 + 36);
         System.out.println(50 / 3);
