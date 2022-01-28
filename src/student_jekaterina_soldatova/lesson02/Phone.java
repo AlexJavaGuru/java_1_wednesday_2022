a48512a7b9f13784e940a1ce43f82e7e1c20b639
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 22:04:02 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson02/Phone.java
@@ -0,0 +1,13 @@
+/*
+Для сдачи работы создайте исправленный класс.
+
+class Phone
+
+}
+ */
+
+package student_jekaterina_soldatova.lesson02;
+
+public class Phone {
+    String number = "Hey, I just met you, and this is crazy\nBut here's my number, so call me, ";
+}
---
+++ src/student_jekaterina_soldatova/lesson02/internTask04.java
@@ -0,0 +1,8 @@
+package student_jekaterina_soldatova.lesson02;
+
+public class internTask04 {
+    public static void main(String[] args) {
+        Phone myObj = new Phone();
+        System.out.println(myObj.number + "maybe");
+    }
+}
