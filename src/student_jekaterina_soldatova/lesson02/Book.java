24ae57f075feea42d40c4a9558fc6b9ebc5054ce
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 21:40:21 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson02/Book.java
@@ -0,0 +1,14 @@
+/*
+        Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+
+class Book {
+
+ */
+package student_jekaterina_soldatova.lesson02;
+
+public class Book {
+    public static void main(String[] args) {
+        System.out.println("Head First Java by Bert Bates and Kathy Sierra");
+    }
+}
