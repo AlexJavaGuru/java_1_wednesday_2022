6e37b8337fc366fceed136e24c011c85b39b41d8
Sergej <sergej777@inbox.lv>
2022-01-28 14:33:13 +0200

    Task18

---
+++ src/student_sergejgsp/task_18/SizeMatters.java
@@ -0,0 +1,9 @@
+package student_sergejgsp.task_18;
+
+    public class SizeMatters {
+        public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+
+    }
+}
