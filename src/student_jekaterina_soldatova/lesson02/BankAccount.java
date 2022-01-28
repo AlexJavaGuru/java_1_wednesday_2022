4b2481158dc3091b159dfebc4b872bb34c3f5cf9
NightCatX <kate.soldatova93@gmail.com>
2022-01-28 22:52:29 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/BankAccount.java
+++ src/student_jekaterina_soldatova/lesson02/BankAccount.java
@@ -3,5 +3,5 @@ package student_jekaterina_soldatova.lesson02;
 public class BankAccount {
     long min = 100000000000L;
     long max = 999999999999L;
-    long x = (long)(Math.random()*(max-min)+min);
+    long x = (long) (Math.random() * (max-min) + min);   //how clean is it? meh? how can it be improved?
 }
