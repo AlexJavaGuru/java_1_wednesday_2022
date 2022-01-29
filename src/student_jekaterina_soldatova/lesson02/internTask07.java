2587b01fcd961e0f06b592a2889a93e69d7349ce
NightCatX <kate.soldatova93@gmail.com>
2022-01-29 02:33:46 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson02/internTask07.java
@@ -0,0 +1,19 @@
+/*
+        Дан код, нужно сделать этот код запускаемым.
+        Для сдачи работы создайте исправленный класс.
+
+class LoginService {
+
+    public void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+
+}
+
+ */
+
+package student_jekaterina_soldatova.lesson02;
+
+public class internTask07 {
+}
