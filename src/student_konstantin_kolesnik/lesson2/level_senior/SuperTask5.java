3032b879525badd3a4c9472a9e042f901326d8f9
CO5M1C <cosinous@gmail.com>
2022-01-29 14:54:30 +0200

    // Rename // Code update... // Fixes

--- src/student_konstantin_kolesnik/lesson2/level_senior/SuperTask5.java
+++ src/student_konstantin_kolesnik/lesson2/level_senior/SuperTask5.java
@@ -6,8 +6,17 @@ public class SuperTask5 {
         //There was a comment.
         System.out.println("Однострочный коментарий начинается с // ");
 
+        //There was a comment.
+        //Here was a commentTwo.
+        System.out.println("Однострочный коментарий начинается с // ");
+
+
         /*Съешь ещё этих мягких французских булок,
         да выпей [же] чаю. 1234567890.*/
         System.out.println("многострочный коментарий начинается с /* и заканчивается */");
+
+
+
+
     }
 }
