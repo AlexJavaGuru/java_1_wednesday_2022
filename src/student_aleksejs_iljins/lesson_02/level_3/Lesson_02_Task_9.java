de048d37b54a3729ffa23e381800d7ab541765f1
AleksejsIljins <oripx85@gmail.com>
2022-01-29 17:50:46 +0200

    Aleksejs Iljins: lesson 02 task 9

---
+++ src/student_aleksejs_iljins/lesson_02/level_3/Lesson_02_Task_9.java
@@ -0,0 +1,36 @@
+package student_aleksejs_iljins.lesson_02.level_3;
+
+import java.util.Scanner;
+
+public class Lesson_02_Task_9 {
+    public static void main(String[] args) {
+        Scanner firstScanner = new Scanner(System.in);
+
+        System.out.println("Input number:");
+        int number = firstScanner.nextInt();
+
+        int resultOne = number * 1;
+        int resultTwo = number * 2;
+        int resultThree = number * 3;
+        int resultFour = number * 4;
+        int resultFive = number * 5;
+        int resultSix = number * 6;
+        int resultSeven = number * 7;
+        int resultEight = number * 8;
+        int resultNine = number * 9;
+        int resultTen = number * 10;
+
+        System.out.println(resultOne);
+        System.out.println(resultTwo);
+        System.out.println(resultThree);
+        System.out.println(resultFour);
+        System.out.println(resultFive);
+        System.out.println(resultSix);
+        System.out.println(resultSeven);
+        System.out.println(resultEight);
+        System.out.println(resultNine);
+        System.out.println(resultTen);
+
+    }
+
+}
