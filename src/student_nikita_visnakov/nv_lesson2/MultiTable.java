014ef82110176d14e87c2b18b4146c23f8b0abf1
VsNicko <n.visnakovs@inbox.lv>
2022-01-30 00:33:03 +0200

    Lesson2 - Level 1-8

---
+++ src/student_nikita_visnakov/nv_lesson2/MultiTable.java
@@ -0,0 +1,31 @@
+package student_nikita_visnakov.nv_lesson2;
+
+import java.util.Scanner;
+
+public class MultiTable {
+
+    public static void main(String[] args) {
+
+        Scanner numScanner = new Scanner(System.in);
+        System.out.println("Say me your favorite number !");
+        int  numA = numScanner.nextInt();
+        System.out.println(" counting: ");
+        int numB = 1;
+        int numC = 2;
+        int numD = 3;
+        int numE = 4;
+        int numF = 5;
+        int numG = 6;
+        int numH = 7;
+        int numI = 8;
+        int numJ = 9;
+        int numK= 10;
+
+
+
+
+
+
+
+    }
+}
