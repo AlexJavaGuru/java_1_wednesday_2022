b31069de8567831cc17485203a4604e08bee656b
Prolog952 <prolog952@gmail.com>
2022-01-30 16:13:23 +0200

    VL@D:  Razbor String  30/01

---
+++ src/student_vlad_stus/Lesson_1/StringExmaple.java
@@ -0,0 +1,9 @@
+package student_vlad_stus.Lesson_1;
+
+public class StringExmaple {
+    public static void main(String[] args) {
+
+
+        String someString = "Hello World! ";
+        System.out.println(someString);
+}
