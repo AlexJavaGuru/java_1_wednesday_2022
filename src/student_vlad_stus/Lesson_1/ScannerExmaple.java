b8919a0a5500a06acb6ed49550c7af06ecd6f861
Prolog952 <prolog952@gmail.com>
2022-01-30 17:37:55 +0200

    VL@D:  ScannerNextInt-final

---
+++ src/student_vlad_stus/Lesson_1/ScannerExmaple.java
@@ -0,0 +1,17 @@
+package student_vlad_stus.Lesson_1;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class ScannerExmaple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // ввыод на консоль
+        System.out.println("Zadatj znacenije dlja A");
+        int variableA = scanner.nextInt();
+        System.out.println("Zadatj znacenije dlja A");
+        int variableB = scanner.nextInt();
+        int result = variableA + variableB;
+        System.out.println("Vvedite rezultat");
+        System.out.println(result);
+    }
+}
