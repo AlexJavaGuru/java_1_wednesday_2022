15a67d5e23347f166eb3fa71675bb2b27dffdf8f
Prolog952 <prolog952@gmail.com>
2022-01-30 18:20:12 +0200

    VL@D:  Lesson_2_Task_1_30/01

---
+++ src/student_vlad_stus/lesson_2/Task1.java
@@ -0,0 +1,17 @@
+package student_vlad_stus.lesson_2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedite pervoje 4islo ");
+        int sumA = scanner.nextInt();
+        System.out.println("Vvedite vtoroje 4islo ");
+        int sumB = scanner.nextInt();
+        System.out.println(sumA + sumB);
+        System.out.println(sumA - sumB);
+        System.out.println(sumA / sumB);
+        System.out.println(sumA % sumB);
+    }
+}
