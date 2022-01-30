410937405e423898e0bc27d65329d545ad32f959
Prolog952 <prolog952@gmail.com>
2022-01-30 16:39:30 +0200

    VL@D:  Console2

---
+++ src/student_vlad_stus/Lesson_1/ConsoleInAndOut.java
@@ -0,0 +1,12 @@
+package student_vlad_stus.Lesson_1;
+
+public class ConsoleInAndOut {
+    public static void main(String[] args) {
+        int sumA = 10;
+        int sumB = 20;
+        int result = sumA+ sumB;
+
+        System.out.println(result);
+        System.out.println("Calcuation: sumA " + sumA + "+ sumB " + sumB + "Result: " + result  );
+    }
+}
