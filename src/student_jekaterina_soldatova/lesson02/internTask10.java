a28a8449c18b3aba0651ae7939717b76536c6c94
NightCatX <kate.soldatova93@gmail.com>
2022-01-30 17:32:14 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/internTask10.java
+++ src/student_jekaterina_soldatova/lesson02/internTask10.java
@@ -11,9 +11,6 @@ Perimeter is = 47.12388980384689
 Area is = 176.71458676442586
  */
 
-//S = π × r2
-//L = 2*r*π
-//Math.PI*
 package student_jekaterina_soldatova.lesson02;
 
 import java.util.Scanner;
@@ -21,12 +18,11 @@ import java.util.Scanner;
 public class internTask10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double pi = Math.PI;
 
         System.out.println("Enter radius of a circle");
         double r = scanner.nextDouble();
 
-        System.out.println("Perimeter is = " + 2 * r * pi);
-        System.out.println("Area is = " + pi * (r * r));
+        System.out.println("Perimeter is = " + 2 * r * Math.PI);
+        System.out.println("Area is = " + Math.PI * (r * r));
     }
 }
