56e93a1f5062d7eadf141bf4c86b9981626b222a
NightCatX <kate.soldatova93@gmail.com>
2022-01-30 19:36:40 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson02/middleTask18.java
@@ -0,0 +1,17 @@
+/*
+Заставьте эту программу работать!
+
+class SizeMatters {
+    public static void main(String[] args) {
+        system.out.println("Sorry, I have an errors.");
+        system.out.println("Please fix me!")
+    }
+}
+*/
+package student_jekaterina_soldatova.lesson02;
+
+public class middleTask18 {
+    public static void main(String[] args) {
+
+    }
+}
