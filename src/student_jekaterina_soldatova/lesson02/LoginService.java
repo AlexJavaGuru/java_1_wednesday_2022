5035d8a4b63b7c3703b2995dbcfb9831dfafe625
NightCatX <kate.soldatova93@gmail.com>
2022-01-30 15:46:03 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/LoginService.java
+++ src/student_jekaterina_soldatova/lesson02/LoginService.java
@@ -1,7 +1,16 @@
 package student_jekaterina_soldatova.lesson02;
 
 class LoginService {
-    boolean Check(String param, int i) {
-        return param.length() <= i;
+    boolean CheckLength(String input, int i) {
+        return input.length() <= i;
+    }
+
+    boolean CheckChars(String input) {
+        for (int i = 0; i < input.length(); i++){
+            if ((int) input.charAt(i) <= 90) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/student_jekaterina_soldatova/lesson02/internTask07.java
+++ src/student_jekaterina_soldatova/lesson02/internTask07.java
@@ -16,23 +16,31 @@ import java.util.Scanner;
 
 public class internTask07 {
     public static void main(String[] args) {
+        int loginLength = 4;
+        int passwordLength = 6;
+
+
         Scanner scanner = new Scanner(System.in);
         LoginService test = new LoginService();
 
         System.out.println("Your new login:");
         String login = scanner.nextLine();
-        while (test.Check(login, 4)){
-            System.out.println("Should be at least 4 characters long, please choose another");
+        while (test.CheckLength(login, loginLength)){
+            System.out.println("Should be at least " + loginLength + " characters long, please choose another");
             login = scanner.nextLine();
         }
 
         System.out.println("Your new password:");
         String password = scanner.nextLine();
-        while (test.Check(password, 6)){
-            System.out.println("Should be at least 6 characters long, please choose another");
-            password = scanner.nextLine();
+        while (test.CheckLength(password, passwordLength) || test.CheckChars(password)){
+            while (test.CheckLength(password, passwordLength)) {
+                System.out.println("Should be at least " + passwordLength + " characters long, please choose another");
+                password = scanner.nextLine();
+            }
+            while (test.CheckChars(password)){
+                System.out.println("Should contain at least one capital letter or a number or a special character, please choose another");
+                password = scanner.nextLine();
+            }
         }
-
-
     }
 }
