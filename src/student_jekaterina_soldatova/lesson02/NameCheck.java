f3bcc04d685cc7c71162859fad6fb402c2ec0a0a
NightCatX <kate.soldatova93@gmail.com>
2022-01-30 16:19:23 +0200

    commit

--- src/student_jekaterina_soldatova/lesson02/NameCheck.java
+++ src/student_jekaterina_soldatova/lesson02/NameCheck.java
@@ -3,7 +3,7 @@ package student_jekaterina_soldatova.lesson02;
 class NameCheck {
     boolean CheckChars(String name) {
         for (int i = 0; i < name.length(); i++){
-            if ((int) name.charAt(i) <= 90 || ((int) name.charAt(i) > 122 && (int) name.charAt(i) < 192) || ((int) name.charAt(i) > 90 && (int) name.charAt(i) < 97)) {
+            if ((int) name.charAt(i) < 65 || (int) name.charAt(i) > 122 || ((int) name.charAt(i) > 90 && (int) name.charAt(i) < 97)) {
                 return false;
             }
         }
