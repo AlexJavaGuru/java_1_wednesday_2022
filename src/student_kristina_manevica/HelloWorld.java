5d3f43b345c64e007be17b92710c33122b224b3e
Kristina Manevica <uc_tuc@inbox.lv>
2022-01-31 20:53:35 +0200

    Исправила ошибку

--- src/student_kristina_manevica/HelloWorld.java
+++ src/student_kristina_manevica/HelloWorld.java
@@ -3,6 +3,6 @@ package student_kristina_manevica;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");}
-
+        System.out.println("Hello World!!!");
     }
+}
