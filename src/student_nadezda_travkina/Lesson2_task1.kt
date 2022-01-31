f2f2644766673c97cb85700c60634d24950739c5
Nadeezzda <nadezdatravkina@gmail.com>
2022-01-31 07:26:39 +0200

    Nadezda: Lesson 2, task 1, Commit Не мсмогла продожить и закончить, так как в принципе не могу проверить, работает ли код. Консоль пишет "could not find or load class".

---
+++ src/student_nadezda_travkina/Lesson2_task1.kt
@@ -0,0 +1,14 @@
+package student_nadezda_travkina
+
+class Lesson2_task1 {
+    public static void main(String[] args)
+    {Scanner num = new Scaner (System.in);
+        int first, second, result;
+        System.out.print("Enter first num: ");
+        first = num.Next.Int();
+        System.out.print("Enter second num: ");
+        second = num.Next.Int();
+        result = first + second;
+        System.out.print.ln("Result is -" + result)
+            }
+}
