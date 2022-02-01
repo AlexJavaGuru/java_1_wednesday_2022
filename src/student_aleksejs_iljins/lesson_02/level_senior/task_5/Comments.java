b454cf40c4e4bbdb71608d0ae6ba18760268466d
AleksejsIljins <oripx85@gmail.com>
2022-02-01 22:09:40 +0200

    Aleksejs Iljins: lesson 02 level 5-7

---
+++ src/student_aleksejs_iljins/lesson_02/level_senior/task_5/Comments.java
@@ -0,0 +1,13 @@
+package student_aleksejs_iljins.lesson_02.level_senior.task_5;
+
+public class Comments {
+    public static void main(String[] args) {
+        // однострочный внутренний комментарий
+        System.out.println("Однострочный внутренний комментарий: //здесь комментарий");
+        /* пример
+        многострочного
+        комментария
+        */
+        System.out.println("Многострочный внутренний комментарий начинается: /* и заканчивается:  */");
+            }
+        }
