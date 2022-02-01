4bc508b1a2f89aaf87621d663e47c0fb99fd0c18
ArtursMel <melnikov270@gmail.com>
2022-02-01 12:12:32 +0200

    Arturs: lesson2 Level5

---
+++ src/student_arturs_melnikovs/lesson_02/level_5/AttentionPlease.java
@@ -0,0 +1,7 @@
+package student_arturs_melnikovs.lesson_02.level_5;
+
+public class AttentionPlease {
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
