6527c4732bd0026db9a1c8d3f90c672777af2758
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-01 19:45:34 +0200

    Aleksandrs K.: completed lesson 2, level 4, task 13

---
+++ src/student_aleksandrs_korsaks/ak_lesson_2/level_4/TaskThirteen.java
@@ -0,0 +1,10 @@
+package student_aleksandrs_korsaks.ak_lesson_2.level_4;
+
+public class TaskThirteen {
+    public static void main(String[] args) {
+        System.out.println("To run Java code in CMD: ");
+        System.out.println("1) Open command prompt (cmd) on Windows;");
+        System.out.println("2) To compile the program, type the following command and hit enter.\"javac Program_Name.java\";");
+        System.out.println("3) After compilation the .java file gets translated into the .class file(byte code). Now we can run the program. To run the program, type the following command and hit enter: \"java Program_Name\".");
+    }
+}
