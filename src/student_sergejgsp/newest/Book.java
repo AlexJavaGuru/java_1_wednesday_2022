c998cb175cd1f0d9a4dde87e4205d2f470bc19a8
Sergej <sergej777@inbox.lv>
2022-02-01 17:26:38 +0200

    CREATING MESSAGES

--- src/student_sergejgsp/newest/Book.java
+++ src/student_sergejgsp/newest/Book.java
@@ -7,7 +7,7 @@ public class Book {
     public static void main(String[] args) {
         String title = "Сообщение";
         String text = "Continue watch!!!";
-        JOptionPane.showMessageDialog(null,text,title,JOptionPane.WARNING_MESSAGE);
-
+        JOptionPane.showMessageDialog(null,text,title,JOptionPane.INFORMATION_MESSAGE);
+// ERROR_MESSAGE, INFORMATION_MESSAGE, QUESTION_MESSAGE, PLAIN_MESSAGE
     }
 }
