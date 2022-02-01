f7a7e88556c99c465fe69840183d3ed27ab1031b
Sergej <sergej777@inbox.lv>
2022-02-01 13:53:27 +0200

    ST_3

---
+++ src/student_sergejgsp/newest/Book.java
@@ -0,0 +1,11 @@
+package student_sergejgsp.newest;
+
+import javax.swing.JOptionPane;
+
+
+public class Book {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "The first Java program");
+
+    }
+}
--- src/student_sergejgsp/task_11/CalculateFourNumbers.java
+++ src/student_sergejgsp/task_11/CalculateFourNumbers.java
@@ -9,4 +9,5 @@ public class CalculateFourNumbers {
         System.out.println(x / y * z + a);
 
 
-    }}
+    }
+}
