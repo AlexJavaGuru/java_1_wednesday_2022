21d324d8dc72fa44c1ca635da7bf5f631d483a09
Nadeezzda <nadezdatravkina@gmail.com>
2022-02-01 07:09:02 +0200

    Nadezda: lesson 2 task 2 - Commit

---
+++ src/student_nadezda_travkina/Lesson2_task2.kt
@@ -0,0 +1,13 @@
+package student_nadezda_travkina
+
+public class Lesson2_task2 {
+    @kotlin.jvm.JvmStatic
+    fun main(args: Array<String>) {
+        val `in` = Scanner(java.lang.System.`in`)
+        print("Input 1st number: ")
+        `in`.nextDouble()
+        print("Input 2nd number: ")
+        `in`.nextDouble()
+        println()
+    }
+}
