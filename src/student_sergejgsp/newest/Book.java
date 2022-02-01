7e2307e9093783497e4598730c71f753a18addc1
Sergej <sergej777@inbox.lv>
2022-02-01 14:05:18 +0200

    ST_3

--- src/student_sergejgsp/newest/Book.java
+++ src/student_sergejgsp/newest/Book.java
@@ -5,7 +5,7 @@ import javax.swing.JOptionPane;
 
 public class Book {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "The first Java program");
+        JOptionPane.showMessageDialog(null, "Думай, что делаешь!!! Нужно ли это тебе");
 
     }
 }
