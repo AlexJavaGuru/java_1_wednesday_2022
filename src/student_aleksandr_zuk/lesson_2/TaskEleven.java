8d9e2b23d681793912fa8a4f442f4e2f4c92f391
AleksandrZuk <sawkovich@hotmail.com>
2022-02-02 10:59:19 +0200

    Aleksandr Zuk: lesson 2 tasks

--- src/student_aleksandr_zuk/lesson_2/TaskEleven.java
+++ src/student_aleksandr_zuk/lesson_2/TaskEleven.java
@@ -12,5 +12,17 @@ public class TaskEleven {
 
         double variable1 = scanner.nextDouble();
 
+        System.out.println("Second variable");
+
+        double variable2 = scanner.nextDouble();
+
+        System.out.println("Third variable");
+
+        double variable3 = scanner.nextDouble();
+
+        double resultSum = (variable1 + variable2 + variable3) / 3;
+
+        System.out.println((variable1 + " + " + variable2 + " + " + variable3) + " / " + 3 + " = " + resultSum);
+
     }
 }
