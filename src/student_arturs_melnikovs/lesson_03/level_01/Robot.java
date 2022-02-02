05d3e626b522559ee157c7ce4419057cbf20b711
ArtursMel <melnikov270@gmail.com>
2022-02-02 22:16:48 +0200

    Arturs: lesson_3 Level_1

---
+++ src/student_arturs_melnikovs/lesson_03/level_01/Robot.java
@@ -0,0 +1,14 @@
+package student_arturs_melnikovs.lesson_03.level_01;
+
+public class Robot {
+
+    public Robot(){
+
+    }
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
---
+++ src/student_arturs_melnikovs/lesson_03/level_01/RobotDemo.java
@@ -0,0 +1,19 @@
+package student_arturs_melnikovs.lesson_03.level_01;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+        Robot myRobot = new Robot();
+        myRobot.sayHello();
+        myRobot.sayYourName();
+
+        Robot robot1 = new Robot();
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+
+}
