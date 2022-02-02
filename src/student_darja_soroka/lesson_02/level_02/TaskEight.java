71302527865e422c9454686de7c7551f413d0250
DarjaSo <darjasoroka@gmail.com>
2022-02-02 22:29:20 +0200

    Darja: Lesson 2; Level 2; Task 8

--- src/student_darja_soroka/lesson_02/level_02/TaskEight.java
+++ src/student_darja_soroka/lesson_02/level_02/TaskEight.java
@@ -8,7 +8,8 @@ public class TaskEight {
 
         System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
-        // ???
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
 
     }
 }
