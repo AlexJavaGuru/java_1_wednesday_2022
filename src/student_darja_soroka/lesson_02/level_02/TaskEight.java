e1e24be064cd449baa2442f0a47d54eabe88af28
DarjaSo <darjasoroka@gmail.com>
2022-02-02 22:13:00 +0200

    Darja: Lesson 2; Level 2; Task 8 not finished

---
+++ src/student_darja_soroka/lesson_02/level_02/TaskEight.java
@@ -0,0 +1,14 @@
+package student_darja_soroka.lesson_02.level_02;
+
+import java.util.Scanner;
+
+public class TaskEight {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
+        // ???
+
+    }
+}
