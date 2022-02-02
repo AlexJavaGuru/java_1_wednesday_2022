3931bf7162e7733578f5c37d2044f99fe064be8f
Prolog952 <prolog952@gmail.com>
2022-02-02 19:09:01 +0200

    Drop Test 8 zadanije

--- src/student_vlad_stus/Drop/Drop_2022.java
+++ src/student_vlad_stus/Drop/Drop_2022.java
@@ -1,9 +1,13 @@
 package student_vlad_stus.Drop;
 
 
-
+import java.util.Scanner;
 public class Drop_2022 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Vvedite imja polzovotelja  ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Spasibo " + scanner);
     }
 }
