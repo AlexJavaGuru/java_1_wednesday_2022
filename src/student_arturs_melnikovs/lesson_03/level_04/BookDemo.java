1318be8b50b0138c9261e581c1f2441e762b6633
ArtursMel <melnikov270@gmail.com>
2022-02-02 22:48:53 +0200

    Arturs: lesson_3 Level_4

---
+++ src/student_arturs_melnikovs/lesson_03/level_04/BookDemo.java
@@ -0,0 +1,9 @@
+package student_arturs_melnikovs.lesson_03.level_04;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
