d3b12b0ca0396a409f3b9922e5ff35afb0956da8
ArtursMel <melnikov270@gmail.com>
2022-02-02 22:29:14 +0200

    Arturs: lesson_3 Level_2

---
+++ src/student_arturs_melnikovs/lesson_03/level_02/TaskSeven.java
@@ -0,0 +1,8 @@
+package student_arturs_melnikovs.lesson_03.level_02;
+
+public class TaskSeven {
+    public static void main(String[] args) {
+        String myName = "Artur";
+        System.out.println(myName);
+    }
+}
