69dae38d02d8470c0ca1273e07039ab11e12b476
ArtursMel <melnikov270@gmail.com>
2022-02-02 22:36:38 +0200

    Arturs: lesson_3 Level_3

--- src/student_arturs_melnikovs/lesson_03/level_01/Robot.java
+++ src/student_arturs_melnikovs/lesson_03/level_01/Robot.java
@@ -2,13 +2,17 @@ package student_arturs_melnikovs.lesson_03.level_01;
 
 public class Robot {
 
-    public Robot(){
+    String name;
 
+    public Robot(String name) {
+        this.name = name;
     }
+
     public void sayHello() {
         System.out.println("Hello!");
     }
+
     public void sayYourName() {
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + this.name);
     }
 }
--- src/student_arturs_melnikovs/lesson_03/level_01/RobotDemo.java
+++ src/student_arturs_melnikovs/lesson_03/level_01/RobotDemo.java
@@ -3,15 +3,15 @@ package student_arturs_melnikovs.lesson_03.level_01;
 public class RobotDemo {
 
     public static void main(String[] args) {
-        Robot myRobot = new Robot();
+        Robot myRobot = new Robot("ROBO");
         myRobot.sayHello();
         myRobot.sayYourName();
 
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot("Biba");
         robot1.sayHello();
         robot1.sayYourName();
 
-        Robot robot2 = new Robot();
+        Robot robot2 = new Robot("Boba");
         robot2.sayHello();
         robot2.sayYourName();
     }
