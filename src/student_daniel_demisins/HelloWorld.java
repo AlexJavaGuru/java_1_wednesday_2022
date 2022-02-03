a49a5d95535882a74fa78487cdd29778c6b42e9f
student_igors <kork2@inbox.lv>
2022-02-03 12:03:13 +0200

    Task11

--- src/student_daniel_demisins/HelloWorld.java
+++ src/student_daniel_demisins/HelloWorld.java
@@ -6,8 +6,5 @@ public class HelloWorld {
         System.out.println("Hello World...");
     }
 }
-public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World...");
-    }
+
