53b7f4d9f4f446385d4a1d917965b74bd720d008
Jelena01 <jelenaboicova01@gmail.com>
2022-02-04 00:07:39 +0200

    Jelena: Lesson 3, Task 1

---
+++ src/student_jelena_boicova/lesson_3/Robot.java
@@ -0,0 +1,11 @@
+package student_jelena_boicova.lesson_3;
+
+public class Robot {
+
+    public static void main(String[] args) {
+
+    }
+        public void sayHello() {
+            // System.out.println("Hello!");
+        }
+    }
