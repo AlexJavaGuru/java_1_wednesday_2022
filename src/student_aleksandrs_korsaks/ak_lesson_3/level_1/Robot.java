672c74c29064597a33d1823e0fff36817f64d02b
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-05 16:39:54 +0200

    Aleksandrs K.: completed lesson 3, level 1, Task 1-2 remake

--- src/student_aleksandrs_korsaks/ak_lesson_3/level_1/Robot.java
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_1/Robot.java
@@ -1,12 +1,8 @@
 package student_aleksandrs_korsaks.ak_lesson_3.level_1;
 
 public class Robot {
-    public static void main(String[] args) {
-        Robot demoRobot = new Robot();
-        demoRobot.sayHello();
-    }
 
-    public Robot (){
+    public Robot() {
 
     }
 
