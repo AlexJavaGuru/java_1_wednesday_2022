ee5a73ab08327a41350d601d3db85b5096c3fa5f
JaKo2577 <jako2577@gmail.com>
2022-02-05 10:22:56 +0200

    Level 3 + Refactoring

--- src/student_aleksandrs_jakovenko/lesson_3/level_1_and_3/RobotDemo.java
+++ src/student_aleksandrs_jakovenko/lesson_3/level_1_and_3/RobotDemo.java
@@ -1,12 +1,12 @@
-package student_aleksandrs_jakovenko.lesson_3.level_1;
+package student_aleksandrs_jakovenko.lesson_3.level_1_and_3;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot myFirstRobot = new Robot();
+        Robot myFirstRobot = new Robot("Rider");
         myFirstRobot.sayHello();
         myFirstRobot.sayYourName();
 
-        Robot mySecondRobot = new Robot();
+        Robot mySecondRobot = new Robot("John");
         mySecondRobot.sayHello();
         mySecondRobot.sayYourName();
     }
