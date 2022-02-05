52747a965406e800b40c8e12d96d0b2efa52bec9
JaKo2577 <jako2577@gmail.com>
2022-02-05 09:51:29 +0200

    Level 1

---
+++ src/student_aleksandrs_jakovenko/lesson_3/Robot.java
@@ -0,0 +1,14 @@
+package student_aleksandrs_jakovenko.lesson_3;
+
+public class Robot {
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
+    }
+}
---
+++ src/student_aleksandrs_jakovenko/lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,15 @@
+package student_aleksandrs_jakovenko.lesson_3.level_1;
+
+import student_aleksandrs_jakovenko.lesson_3.Robot;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot myFirstRobot = new Robot();
+        myFirstRobot.sayHello();
+        myFirstRobot.sayYourName();
+
+        Robot mySecondRobot = new Robot();
+        mySecondRobot.sayHello();
+        mySecondRobot.sayYourName();
+    }
+}
