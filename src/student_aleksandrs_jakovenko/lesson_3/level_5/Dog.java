80cfccfb18708ec912cad7f91e1393d1e2ca1025
JaKo2577 <jako2577@gmail.com>
2022-02-05 12:02:25 +0200

    Task 21

--- src/student_aleksandrs_jakovenko/lesson_3/level_5/Dog.java
+++ src/student_aleksandrs_jakovenko/lesson_3/level_5/Dog.java
@@ -3,6 +3,7 @@ package student_aleksandrs_jakovenko.lesson_3.level_5;
 public class Dog {
     String name = "Richy";
     int age = 5;
+    String color;
 
     void voice(){
 //        Task 18
@@ -11,12 +12,21 @@ public class Dog {
 //        System.out.println("Dog name: " + name);
 //        System.out.println("Dog age: " + age);
 
-        //Task 20
-        System.out.println(name + " " + name + " " + name);
+//        Task 20
+//        System.out.println(name + " " + name + " " + name);
+//        System.out.println("Dog age is: " + age);
+
+        //Task 21
+        System.out.println("Dog name: " + name);
         System.out.println("Dog age is: " + age);
+        System.out.println("Dog color is: " + color);
     }
 
     void happyBirthday(){
         this.age = age + 1;
+    }
+
+    public Dog(String color){
+        this.color = color;
     }
 }
--- src/student_aleksandrs_jakovenko/lesson_3/level_5/DogDemo.java
+++ src/student_aleksandrs_jakovenko/lesson_3/level_5/DogDemo.java
@@ -2,10 +2,11 @@ package student_aleksandrs_jakovenko.lesson_3.level_5;
 
 public class DogDemo {
     public static void main(String[] args) {
-        Dog dog = new Dog();
+        Dog dog = new Dog("Black");
         dog.voice();
-        dog.happyBirthday();
-        dog.voice();
+//        Task20
+//        dog.happyBirthday();
+//        dog.voice();
 
     }
 }
