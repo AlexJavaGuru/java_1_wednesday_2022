0fe4d445f29fdb7f6f11cbcf3a7cdccd667a846b
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-05 18:19:02 +0200

    Aleksandrs K.: completed lesson 3, level 3, Task 11-13

--- src/student_aleksandrs_korsaks/ak_lesson_3/level_1/Robot.java
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_1/Robot.java
@@ -2,8 +2,14 @@ package student_aleksandrs_korsaks.ak_lesson_3.level_1;
 
 public class Robot {
 
+    String name;
+
     public Robot() {
 
+    }
+
+    public Robot(String robotName) {
+        this.name = robotName;
     }
 
     public void sayHello() {
@@ -11,6 +17,6 @@ public class Robot {
     }
 
     public void sayYourName() {
-        System.out.println("My name is ROBO");
+        System.out.println("My name is " + name);
     }
 }
--- src/student_aleksandrs_korsaks/ak_lesson_3/level_1/RobotDemo.java
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_1/RobotDemo.java
@@ -3,20 +3,17 @@ package student_aleksandrs_korsaks.ak_lesson_3.level_1;
 public class RobotDemo {
     public static void main(String[] args) {
 
-        Robot demoRobot = new Robot();
-        System.out.println("demoRobot");
+        Robot demoRobot = new Robot("Demo");
         demoRobot.sayHello();
         demoRobot.sayYourName();
         System.out.println("");
 
-        Robot robotOne = new Robot();
-        System.out.println("robotOne");
+        Robot robotOne = new Robot("Rider");
         robotOne.sayHello();
         robotOne.sayYourName();
         System.out.println("");
 
-        Robot robotTwo = new Robot();
-        System.out.println("robotTwo");
+        Robot robotTwo = new Robot("John");
         robotTwo.sayHello();
         robotTwo.sayYourName();
         System.out.println("");
