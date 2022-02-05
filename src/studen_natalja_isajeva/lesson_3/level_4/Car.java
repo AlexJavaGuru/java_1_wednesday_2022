99da600b626650413ba52a70fa063110b4a2ee9c
Ната <natiscrossstitch@gmail.com>
2022-02-05 18:22:26 +0200

    Natalja: Lesson3 level 1_4

---
+++ src/studen_natalja_isajeva/lesson_3/level_4/Car.java
@@ -0,0 +1,14 @@
+package studen_natalja_isajeva.lesson_3.level_4;
+
+public class Car {
+    String model;
+
+    public Car(String model) {
+        this.model = model;
+    }
+
+    String getModel() {
+        return this.model;
+
+    }
+}
---
+++ src/studen_natalja_isajeva/lesson_3/level_4/CarDemo.java
@@ -0,0 +1,9 @@
+package studen_natalja_isajeva.lesson_3.level_4;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+}
