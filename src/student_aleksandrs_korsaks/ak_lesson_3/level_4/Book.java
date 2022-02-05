eec9252b414f70cbcafbaf52f79940fde376e4c5
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-05 18:24:51 +0200

    Aleksandrs K.: completed lesson 3, level 4, Task 14

---
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_4/Book.java
@@ -0,0 +1,15 @@
+package student_aleksandrs_korsaks.ak_lesson_3.level_4;
+
+public class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
---
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_4/BookDemo.java
@@ -0,0 +1,11 @@
+package student_aleksandrs_korsaks.ak_lesson_3.level_4;
+
+public class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
+
