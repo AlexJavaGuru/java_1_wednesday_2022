153b84d9fdf7102bd55ada808a292b1f21144037
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-05 18:28:28 +0200

    Aleksandrs K.: completed lesson 3, level 4, Task 15

---
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_4/PhoneDemo.java
@@ -0,0 +1,12 @@
+package student_aleksandrs_korsaks.ak_lesson_3.level_4;
+
+public class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
+
