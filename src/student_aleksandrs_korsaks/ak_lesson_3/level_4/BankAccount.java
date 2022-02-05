e53de1b3f2219c9b0a9f1150e8f48bfe01399ee4
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-05 18:45:19 +0200

    Aleksandrs K.: completed lesson 3, level 4, Task 17

---
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_4/BankAccount.java
@@ -0,0 +1,28 @@
+package student_aleksandrs_korsaks.ak_lesson_3.level_4;
+
+public class BankAccount {
+
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+    Integer getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
+
