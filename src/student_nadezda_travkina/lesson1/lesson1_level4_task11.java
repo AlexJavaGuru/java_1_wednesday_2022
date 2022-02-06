717aa3e17271f458972df68ab5f2b02f770e944e
Nadeezzda <nadezdatravkina@gmail.com>
2022-02-06 14:11:28 +0200

    Nadezda:lesson1, task 1 - не понимаю в чем ошибка

---
+++ src/student_nadezda_travkina/lesson1/lesson1_level4_task11.java
@@ -0,0 +1,14 @@
+package student_nadezda_travkina.lesson1;
+
+public class lesson1_level4_task11 {
+    int a = 74;
+    int b = 36;
+    int sum = a + b;
+     System.out.println(sum)
+
+    int c = 50;
+    int d = 3;
+    int divide = c/d;
+        System.out.println(divide)
+
+}
