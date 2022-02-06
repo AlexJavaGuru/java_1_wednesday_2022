306eb1b0dca683cfb6054e6048ca868d062fb8b4
Jelena01 <jelenaboicova01@gmail.com>
2022-02-07 01:54:48 +0200

    Jelena: Lesson 3, Level 5

---
+++ src/student_jelena_boicova/lesson_3/Dog.java
@@ -0,0 +1,20 @@
+package student_jelena_boicova.lesson_3;
+
+public class Dog {
+
+  String nickname;
+
+    Dog(String dogNickname){
+        this.nickname = dogNickname;
+
+}
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void voice(){
+        System.out.println("Max, Max,Max");
+
+    }
+}
---
+++ src/student_jelena_boicova/lesson_3/DogDemo.java
@@ -0,0 +1,10 @@
+package student_jelena_boicova.lesson_3;
+
+public class DogDemo {
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Max");
+        myDog.voice();
+    }
+
+}
