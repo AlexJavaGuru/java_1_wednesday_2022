d5428e9a07b5619902458d833191805d3f63519a
Jelena01 <jelenaboicova01@gmail.com>
2022-02-06 22:49:56 +0200

    Jelena: Lesson 3, Level 1

--- src/student_jelena_boicova/lesson_3/Robot.java
+++ src/student_jelena_boicova/lesson_3/Robot.java
@@ -1,11 +1,18 @@
 package student_jelena_boicova.lesson_3;
 
 public class Robot {
+    String robotName;
 
-    public static void main(String[] args) {
-        Robot robot = new Robot();
+    public void sayHello() {
+        System.out.println("Hello!");
     }
-        public void sayHello() {
-            // System.out.println("Hello!");
+
+    public Robot(){
+
+    }
+    public void sayYourName(){
+        System.out.println("My name is ROBO");
+
+
         }
     }
---
+++ src/student_jelena_boicova/lesson_3/RobotDemo.java
@@ -0,0 +1,15 @@
+package student_jelena_boicova.lesson_3;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot myFirstRobot = new Robot();
+        myFirstRobot.sayHello();
+        myFirstRobot.sayYourName();
+
+        Robot mySecondRobot = new Robot();
+        mySecondRobot.sayHello();
+        mySecondRobot.sayYourName();
+    }
+}
