40e3e4fd0ba5a5b11a344f2d4285026e46b19191
NightCatX <kate.soldatova93@gmail.com>
2022-02-06 17:29:49 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson_3/intern/Robot.java
@@ -0,0 +1,19 @@
+/*
+        Создайте класс робот (Robot).
+
+        Научите вашего робота "говорить" привет.
+
+        Создайте в классе Robot метод со следующей сигнатурой:
+
+public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+        }
+*/
+
+package student_jekaterina_soldatova.lesson_3.intern;
+
+public class Robot {
+    public void sayHello() {
+        System.out.println("Hello there");
+    }
+}
---
+++ src/student_jekaterina_soldatova/lesson_3/intern/Task01.java
@@ -0,0 +1,20 @@
+/*
+        Создайте класс робот (Robot).
+
+        Научите вашего робота "говорить" привет.
+
+        Создайте в классе Robot метод со следующей сигнатурой:
+
+public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+        }
+*/
+
+package student_jekaterina_soldatova.lesson_3.intern;
+
+public class Task01 {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.sayHello();
+    }
+}
