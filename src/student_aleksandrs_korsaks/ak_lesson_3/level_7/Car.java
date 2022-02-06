0e059a4c4560574a68b336d1979579ea66c76068
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-06 20:45:54 +0200

    Aleksandrs K.: completed lesson 3, level, Task 29

---
+++ src/student_aleksandrs_korsaks/ak_lesson_3/level_7/Car.java
@@ -0,0 +1,36 @@
+package student_aleksandrs_korsaks.ak_lesson_3.level_7;
+
+public class Car {
+    private final String carModel;
+    private final int yearOfProduction;
+    private int carPrice;
+    private int carOdometr;
+    private String carColour;
+
+    public Car(String carModel, int yearOfProduction, String carColour, int carPrice, int carOdometr) {
+        this.carModel = carModel;
+        this.yearOfProduction = yearOfProduction;
+        this.carColour = carColour;
+        this.carPrice = carPrice;
+        this.carOdometr = carOdometr;
+    }
+
+    public void setCarPrice(int carPrice) {
+        this.carPrice = carPrice;
+    }
+
+    public void setCarColour(String carColour) {
+        this.carColour = carColour;
+    }
+
+    @Override
+    public String toString() {
+        return "Car {" +
+                "Model - '" + carModel + '\'' +
+                "; Year of production - " + yearOfProduction +
+                "; Colour - " + carColour +
+                "; Price - " + carPrice +
+                "; Odometr - " + carOdometr +
+                '}';
+    }
+}
