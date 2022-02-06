6c0ef015f7698f4f23c0cb92071fc4d651fcb79d
DarjaSo <darjasoroka@gmail.com>
2022-02-06 15:14:15 +0200

    Darja: Lesson 2; Level 6

---
+++ src/student_darja_soroka/lesson_02/level_06/AttentionPlease.java
@@ -0,0 +1,8 @@
+package student_darja_soroka.lesson_02.level_06;
+
+public class AttentionPlease {
+
+    public static void main(String[] args) {
+        System.out.println("Code outside the method will not compile!");
+    }
+}
