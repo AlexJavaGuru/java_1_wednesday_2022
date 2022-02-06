df7ea1d228e91924f4a53672aa77e69538d1fdd1
NightCatX <kate.soldatova93@gmail.com>
2022-02-06 23:53:11 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson_3/senior/BankAccount.java
@@ -0,0 +1,20 @@
+package student_jekaterina_soldatova.lesson_3.senior;
+
+class BankAccount {
+    String owner;
+    int money;
+
+    BankAccount(String owner, int money) {
+        this.owner = owner;
+        this.money = money;
+    }
+
+    String getOwner() {
+        return this.owner;
+    }
+
+    int getMoney() {
+        return this.money;
+    }
+
+}
