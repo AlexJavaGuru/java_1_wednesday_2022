533d35294904f377a4474562c7e367b819a45af7
NightCatX <kate.soldatova93@gmail.com>
2022-02-06 22:15:15 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson_3/senior/Circle.java
@@ -0,0 +1,13 @@
+package student_jekaterina_soldatova.lesson_3.senior;
+
+class Circle {
+    double radius;
+
+    Circle (double radius) {
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
