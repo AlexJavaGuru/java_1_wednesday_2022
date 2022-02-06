abc091493626377a2d288746ee8aca1ad3812170
Jelena01 <jelenaboicova01@gmail.com>
2022-02-07 00:37:12 +0200

    Jelena: Lesson 3, Level 4

---
+++ src/student_jelena_boicova/lesson_3/Car.java
@@ -0,0 +1,14 @@
+package student_jelena_boicova.lesson_3;
+
+public class Car {
+
+    String model;
+
+        public Car(String model) {
+            this.model = model;
+        }
+        String getModel () {
+            return this.model;
+        }
+
+    }
