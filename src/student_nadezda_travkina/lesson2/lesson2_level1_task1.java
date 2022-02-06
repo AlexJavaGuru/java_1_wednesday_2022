5f636a379c65409ea90b3ca74e90941d2685ff54
Nadeezzda <nadezdatravkina@gmail.com>
2022-02-06 14:29:37 +0200

    Nadezda:lesson1, task 1 - опять run не дает результата, также, так как " /n " закрашивает красным, видимо мой метод выведения резльтата на новую строку не работает, почему? И что тогда имелось в ввиду как правильный метод?

---
+++ src/student_nadezda_travkina/lesson2/lesson2_level1_task1.java
@@ -0,0 +1,18 @@
+package student_nadezda_travkina.lesson2;
+
+import java.util.Scanner;
+
+public class lesson2_level1_task1 {
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Value for A:");
+        int variableA = myScanner.nextInt();
+        System.out.println("Value for B:");
+        int variableB = myScanner.nextInt();
+        double sum = variableA + variableB /n ;
+        double minus = variableA - variableB /n ;
+        double multiply = variableA * variableB /n ;
+        double divide = variableA / variableB / n ;
+}
