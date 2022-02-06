4e981b7e4c566d49905ddfceacf34b1f55196c53
AleksandrZuk <sawkovich@hotmail.com>
2022-02-06 12:57:22 +0200

    Aleksandr Zuk: Lesson 3 level 1

---
+++ src/student_aleksandr_zuk/lesson_3/RobotDemo.java
@@ -0,0 +1,20 @@
+package student_aleksandr_zuk.lesson_3;
+
+
+public class RobotDemo {
+
+
+    public static void main(String[] args) {
+
+       Robot myFirstRobot = new Robot();
+       myFirstRobot.sayHello();
+       myFirstRobot.sayYourName();
+
+       Robot mySecondRobot = new Robot();
+       mySecondRobot.sayHello();
+       mySecondRobot.sayYourName();
+
+
+
+    }
+}
