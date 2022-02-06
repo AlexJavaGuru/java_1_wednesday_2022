7b25c85e47abb8d928751225a966c0ab81fc40e7
Jelena01 <jelenaboicova01@gmail.com>
2022-02-07 00:57:42 +0200

    Jelena: Lesson 3, Level 4

---
+++ src/student_jelena_boicova/lesson_3/BankAccount.java
@@ -0,0 +1,26 @@
+package student_jelena_boicova.lesson_3;
+
+public class BankAccount {
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
