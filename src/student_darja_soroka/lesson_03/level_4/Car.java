0efada472e73881af44c1bb24125c0d78cb9ac9c
DarjaSo <darjasoroka@gmail.com>
2022-02-07 13:38:12 +0200

    Darja: Lesson 3; Level 4

---
+++ src/student_darja_soroka/lesson_03/level_4/Car.java
@@ -0,0 +1,14 @@
+package student_darja_soroka.lesson_03.level_4;
+
+public class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+}
