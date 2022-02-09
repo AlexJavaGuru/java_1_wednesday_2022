55b1f1b608cc1c8a8c00453bc8e9e50b7ac7842f
Vladislav <vladislavsdeineka@gmail.com>
2022-02-09 13:02:17 +0200

    VladislavD: Lesson3NotFinished

---
+++ src/student_vladislav_deineka/lesson_3/task_2/MyName.java
@@ -0,0 +1,10 @@
+package student_vladislav_deineka.lesson_3.task_2;
+
+public class MyName {
+
+    public static void main(String[] args) {
+        String MyName = "Vlad";
+        System.out.println(MyName);
+        
+    }
+}
---
+++ src/student_vladislav_deineka/lesson_3/task_3/BookDemo.java
@@ -0,0 +1,10 @@
+package student_vladislav_deineka.lesson_3.task_3;
+
+public class BookDemo {
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
+
