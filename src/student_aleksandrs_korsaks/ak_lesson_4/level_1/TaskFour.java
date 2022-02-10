436d82cba2ffdf7affe95cfad98de38d9520ecd5
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-10 22:01:51 +0200

    Aleksandrs K.: completed lesson 4, level 1, Task 5

--- src/student_aleksandrs_korsaks/ak_lesson_4/level_1/TaskFour.java
+++ src/student_aleksandrs_korsaks/ak_lesson_4/level_1/TaskFour.java
@@ -2,7 +2,7 @@ package student_aleksandrs_korsaks.ak_lesson_4.level_1;
 
 import java.util.Scanner;
 
-public class TaskFour {
+class TaskFour {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input integer");
