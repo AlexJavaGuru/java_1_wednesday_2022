e6725a9a225d77632d7935792985b47fd934fdca
Jelena01 <jelenaboicova01@gmail.com>
2022-02-10 22:47:54 +0200

    Jelena: Lesson 4, Level 1

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Task1.java
@@ -0,0 +1,25 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int x = -6;
+
+
+    if (x < 0) {
+        System.out.println("Entered number is negative");
+        // negative
+    } else {
+        System.out.println("Entered number is positive");
+        // positive
+    }
+
+
+    }
+ }
+
