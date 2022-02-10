a1c04dcb3a5892a9615f204988773acc1097f2d8
JaKo2577 <jako2577@gmail.com>
2022-02-10 20:22:43 +0200

    Level 5

--- src/student_aleksandrs_jakovenko/lesson_4/level_4/CalculatorTest.java
+++ src/student_aleksandrs_jakovenko/lesson_4/level_4/CalculatorTest.java
@@ -1,6 +1,6 @@
 package student_aleksandrs_jakovenko.lesson_4.level_4;
 
-public class CalculatorTest {
+class CalculatorTest {
     public static void main(String[] args) {
         CalculatorTest calculatorTest = new CalculatorTest();
         calculatorTest.sumTest();
