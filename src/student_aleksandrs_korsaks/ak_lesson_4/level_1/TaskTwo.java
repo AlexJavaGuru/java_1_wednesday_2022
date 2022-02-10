ead351aade155696d5f7c774d552d01a00499235
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-10 20:58:38 +0200

    Aleksandrs K.: completed lesson 4, level 1, Task 2 + Task 1 remastered

--- src/student_aleksandrs_korsaks/ak_lesson_4/level_1/TaskTwo.java
+++ src/student_aleksandrs_korsaks/ak_lesson_4/level_1/TaskTwo.java
@@ -2,16 +2,19 @@ package student_aleksandrs_korsaks.ak_lesson_4.level_1;
 
 import java.util.Scanner;
 
-class TaskOne {
+class TaskTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please input integer");
         int inputNumber = scanner.nextInt();
 
-        if (inputNumber > 0) {
-            System.out.println("You`ve entered positive");
+        if (inputNumber == 0) {
+            System.out.println("You`ve entered 0");
+        } else if (inputNumber > 0) {
+            System.out.println("You`ve entered positive integer");
         } else {
             System.out.println("You`ve entered negative");
         }
     }
 }
+
