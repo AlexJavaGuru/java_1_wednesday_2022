f1022d14ffb7f7079c23464ef30a914332eedddc
Wanxezar <zenadik@inbox.lv>
2022-02-10 15:12:40 +0200

    Daniel Demisins

---
+++ src/student_daniel_demisins/HW_Lesson2/Lvl_1_Task_1.java
@@ -0,0 +1,58 @@
+package student_daniel_demisins.HW_Lesson2;
+
+import java.util.Scanner;
+
+public class Lvl_1_Task_1 {
+
+    public static void main(String[] args) {
+        Scanner plusScanner = new Scanner(System.in);
+        System.out.println("Value for A");
+        int plusA = plusScanner.nextInt();
+
+        System.out.println("Value for B");
+        int plusB = plusScanner.nextInt();
+        int plusresult = plusA + plusB;
+
+        System.out.println("Value for result");
+        System.out.println(plusresult);
+
+
+
+        Scanner minusScanner = new Scanner(System.in);
+        System.out.println("Value for A");
+        int minusA = minusScanner.nextInt();
+
+        System.out.println("Value for B");
+        int minusB = minusScanner.nextInt();
+        int minusresult = minusA - minusB;
+
+        System.out.println("Value for result");
+        System.out.println(minusresult);
+
+
+
+        Scanner delenieScanner = new Scanner(System.in);
+        System.out.println("Value for A");
+        int delenieA = delenieScanner.nextInt();
+
+        System.out.println("Value for B");
+        int delenieB = delenieScanner.nextInt();
+        int delenieresult = delenieA / delenieB;
+
+        System.out.println("Value for result");
+        System.out.println(delenieresult);
+
+
+
+        Scanner umnozenieScanner = new Scanner(System.in);
+        System.out.println("Value for A");
+        int umnozenieA = umnozenieScanner.nextInt();
+
+        System.out.println("Value for B");
+        int umnozenieB = umnozenieScanner.nextInt();
+        int umnozenieresult = umnozenieA * umnozenieB;
+
+        System.out.println("Value for result");
+        System.out.println(umnozenieresult);
+    }
+}
---
+++ src/student_daniel_demisins/HW_Lesson2/Lvl_2_Task_06_Turtle.java
@@ -0,0 +1,7 @@
+package student_daniel_demisins.HW_Lesson2;
+
+public class Lvl_2_Task_06_Turtle {
+    public static void main(String[] args) {
+
+    }
+}
