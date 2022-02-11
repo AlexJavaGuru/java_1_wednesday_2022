169cf91ce9311e25f01e636be7408327f5be2995
Jelena01 <jelenaboicova01@gmail.com>
2022-02-11 18:31:40 +0200

    Jelena: Lesson 4, Level 2

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Task7.java
@@ -0,0 +1,20 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter two numbers");
+        int a = 70;
+        int b = 70;
+
+        if (a == b) {
+            System.out.println("Numbers are equals");
+        } else {
+            System.out.println("Numbers are different");
+        }
+    }
+}
+
