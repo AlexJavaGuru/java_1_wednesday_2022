6f6744311067675686f9a0f911f1a41cc29039a1
Jelena01 <jelenaboicova01@gmail.com>
2022-02-11 18:21:17 +0200

    Jelena: Lesson 4, Level 2

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Task6.java
@@ -0,0 +1,20 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter two numbers");
+        int a = 100;
+        int b = 70;
+
+        if (a < b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
+
