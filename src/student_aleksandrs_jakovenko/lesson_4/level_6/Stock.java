737f3d1fdfba86dbb06efcb79bd78548b5571298
JaKo2577 <jako2577@gmail.com>
2022-02-11 15:43:19 +0200

    Task 17

---
+++ src/student_aleksandrs_jakovenko/lesson_4/level_6/Stock.java
@@ -0,0 +1,38 @@
+package student_aleksandrs_jakovenko.lesson_4.level_6;
+
+class Stock {
+    String companyName;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    public Stock (String companyName, int currentPrice){
+        this.companyName = companyName;
+        this.currentPrice = currentPrice;
+        minPrice = currentPrice;
+        maxPrice = currentPrice;
+    }
+
+    void updatePrice(int newPrice){
+        if (newPrice > maxPrice){
+            maxPrice = newPrice;
+        }
+        else if (newPrice < minPrice){
+            minPrice = newPrice;
+        }
+        else currentPrice = newPrice;
+    }
+    String getPriceInformation(){
+        String getInfo = ("Company name = " + companyName + ", " + "Current price = " + currentPrice + ", " + "Min price = " + minPrice + ", " + "Max price = " + maxPrice);
+        return getInfo;
+    }
+    int getMaxPrice() {
+        return maxPrice;
+    }
+    int getMinPrice() {
+        return minPrice;
+    }
+    int getCurrentPrice() {
+        return currentPrice;
+    }
+}
