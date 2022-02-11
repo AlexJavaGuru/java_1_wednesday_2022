9f6be5b69fe4162efb812033909be563b172e161
Jekaterina <kate.soldatova93@gmail.com>
2022-02-11 23:05:14 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson_1/Task1.java
@@ -0,0 +1,24 @@
+/*
+Написать программу, которая запрашивает у
+пользователя
+целое число и выводит на консоль
+положительное оно или отрицательное.
+ */
+
+package student_jekaterina_soldatova.lesson_1;
+
+import java.util.Scanner;
+
+class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberToCheck = scanner.nextInt();
+        if (numberToCheck > 0) {
+            System.out.println("The number is positive");
+        } else if (numberToCheck == 0) {
+            System.out.println("The number is 0");
+        } else {
+            System.out.println("The number is negative");
+        }
+    }
+}
