173a3bbf0eec09f98c4a54dfb10a4f04f721c706
student_igors <kork2@inbox.lv>
2022-02-12 07:35:29 +0200

    Lesson_02

--- src/student_igors/Lesson_2_Scanner_in.java
+++ src/student_igors/Lesson_2_Scanner_in.java
@@ -2,17 +2,16 @@ package student_igors;
 
 import java.util.Scanner;
 
-public class Lesson_2_Task_1 {
+public class Lesson_2_Scanner_in {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int variableA = scanner.nextInt();
         int variableB = scanner.nextInt();
-        int result = variableA + variableB;
+        int result = variableA / variableB;
 
         System.out.println("result");
-
     }
 
 
