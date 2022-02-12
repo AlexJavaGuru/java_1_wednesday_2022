197d45945e61e0a4e1556546d1148bd1358f8c9a
student_igors <kork2@inbox.lv>
2022-02-12 09:57:24 +0200

    Lesson_02_Task1

---
+++ src/student_igors/Lesson2/Task1.java
@@ -0,0 +1,20 @@
+package student_igors.Lesson2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    Scanner scanner = new Scanner(System.in);{
+    int variableA = scanner.nextInt();
+    int variableB = scanner.nextInt();
+    int result1 = variableA / variableB;
+    int result2 = variableA * variableB;
+    int result3 = variableA + variableB;
+    int result4 = variableA - variableB;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+
+     }
+}
