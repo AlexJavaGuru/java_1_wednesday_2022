5e5925c106a5cfd572ec7954e99569bc7986b805
student_igors <kork2@inbox.lv>
2022-02-12 21:08:29 +0200

    AscNumberOne

--- src/student_igors/AscNumber/AscNumberOne.java
+++ src/student_igors/AscNumber/AscNumberOne.java
@@ -1,7 +1,25 @@
 package student_igors.AscNumber;
 
+import java.util.Scanner;
+
 public class AscNumberOne {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 1st decimal number");
+        double firstDoubleNumber = scanner.nextDouble();
+        System.out.println("Enter 2nd decimal number");
+        double secondDoubleNumber = scanner.nextDouble();
+
+        double sum = firstDoubleNumber + secondDoubleNumber;
+        double sub = firstDoubleNumber - secondDoubleNumber;
+        double mult = firstDoubleNumber * secondDoubleNumber;
+        double div = firstDoubleNumber / secondDoubleNumber;
+
+        System.out.println(firstDoubleNumber + " + " + secondDoubleNumber + " = " + Math.round(sum * 100.0) / 100.0);
+        System.out.println(firstDoubleNumber + " - " + secondDoubleNumber + " = " + Math.round(sub * 100.0) / 100.0);
+        System.out.println(firstDoubleNumber + " * " + secondDoubleNumber + " = " + Math.round(mult * 100.0) / 100.0);
+        System.out.println(firstDoubleNumber + " / " + secondDoubleNumber + " = " + Math.round(div * 100.0) / 100.0);
     }
+
 }
