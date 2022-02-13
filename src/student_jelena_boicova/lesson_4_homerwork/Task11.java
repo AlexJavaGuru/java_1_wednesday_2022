50ee7d33a66c91c9299201b3642c38d433e03431
Jelena01 <jelenaboicova01@gmail.com>
2022-02-13 21:38:55 +0200

    Jelena: Lesson 4, Level 4

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Task11.java
@@ -0,0 +1,22 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+}
+    class Calculator {
+
+        public int sum(int firstNumber, int secondNumber) {
+            return firstNumber + secondNumber;
+        }
+        public int sub(int firstNumber, int secondNumber) {
+            return firstNumber - secondNumber;
+        }
+        public int mul(int firstNumber, int secondNumber) {
+            return firstNumber * secondNumber;
+        }
+        public int div(int firstNumber, int secondNumber) {
+            return firstNumber / secondNumber;
+        }
+    }
+}
