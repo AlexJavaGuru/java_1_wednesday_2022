0097e9da61e491e6899335d5f5e8a9bbb4874044
Ната <natiscrossstitch@gmail.com>
2022-02-13 16:11:21 +0200

    Natalja: Lesson4 level 1_5 02

--- src/studen_natalja_isajeva/lesson_4/level_4/Calculator.java
+++ src/studen_natalja_isajeva/lesson_4/level_4/Calculator.java
@@ -5,22 +5,48 @@ public class Calculator {
     //task 12
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
+
     }
+
     public int sub(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
+
     }
+
     public int mul(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
+
     }
+
     public int div(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
 //Task 13
 
     public boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
+        return number % 2 == 0;
+    }
+
+    // Task 14
+    public int maxOfTwoNumbers(int firstNumber, int secondNumber) {
+       if (firstNumber <= secondNumber) {
+            return secondNumber;
+        } else {
+            return firstNumber;
         }
-        return false;
+
     }
+
+    public int maxOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        if (secondNumber <= firstNumber && thirdNumber <= firstNumber ) {
+            return firstNumber;
+        } else if (firstNumber <= secondNumber && thirdNumber <= secondNumber){
+            return secondNumber;
+        } else {
+            return thirdNumber;
+        }
+
     }
+}
+
+
--- src/studen_natalja_isajeva/lesson_4/level_4/CalculatorDemo.java
+++ src/studen_natalja_isajeva/lesson_4/level_4/CalculatorDemo.java
@@ -24,6 +24,10 @@ public class CalculatorDemo {
 
   //Task 13
         System.out.println(calculator.isEven (10));
-
+  // Task 14
+        System.out.println(calculator.maxOfTwoNumbers(10, 20));
+  // Task 15
+        System.out.println(calculator.maxOfThreeNumbers(10, 20, 5));
     }
 }
+
