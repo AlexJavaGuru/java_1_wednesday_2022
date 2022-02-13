33fd2caabfb24d7906a6c7a510cc7fdba5eea0d1
JaKo2577 <jako2577@gmail.com>
2022-02-13 19:31:00 +0200

    Finally circle edited

--- src/student_aleksandrs_jakovenko/lesson_3/level_7/CircleDemo.java
+++ src/student_aleksandrs_jakovenko/lesson_3/level_7/CircleDemo.java
@@ -2,8 +2,8 @@ package student_aleksandrs_jakovenko.lesson_3.level_7;
 
 public class CircleDemo {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        double result = circle.calculateArea(3.5);
+        Circle circle = new Circle(3.5);
+        double result = circle.calculateArea();
         System.out.println("Circle area = " + result);
     }
 }
