93d0a03d94bfa453c0273b7d78e99a1217bfad5e
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-13 16:44:49 +0200

    Aleksandrs K.: completed lesson 4, level 4, Task 12

--- src/student_aleksandrs_korsaks/ak_lesson_4/level_4/Calculator.java
+++ src/student_aleksandrs_korsaks/ak_lesson_4/level_4/Calculator.java
@@ -1,6 +1,6 @@
 package student_aleksandrs_korsaks.ak_lesson_4.level_4;
 
-public class Calculator {
+class Calculator {
 
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
@@ -14,7 +14,7 @@ public class Calculator {
         return firstNumber * secondNumber;
     }
 
-    public double div(int firstNumber, int secondNumber) {
-        return firstNumber / secondNumber;
+    public double div(double firstNumber, double secondNumber) {
+        return Math.round((firstNumber / secondNumber) * 100.0) / 100.0;
     }
 }
---
+++ src/student_aleksandrs_korsaks/ak_lesson_4/level_4/CalculatorDemo.java
@@ -0,0 +1,19 @@
+package student_aleksandrs_korsaks.ak_lesson_4.level_4;
+
+import java.util.Scanner;
+
+class CalculatorDemo {
+    public static void main(String[] args) {
+        Calculator newCalc = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input 1st number");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Please input 2st number");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + newCalc.sum(firstNumber, secondNumber));
+        System.out.println(firstNumber + " - " + secondNumber + " = " + newCalc.sub(firstNumber, secondNumber));
+        System.out.println(firstNumber + " * " + secondNumber + " = " + newCalc.mul(firstNumber, secondNumber));
+        System.out.println(firstNumber + " / " + secondNumber + " = " + newCalc.div(firstNumber, secondNumber));
+    }
+}
