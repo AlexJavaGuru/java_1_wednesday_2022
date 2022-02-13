ade0ad5c74f272ad1b2355f8ddf7680d663ccf06
Jelena01 <jelenaboicova01@gmail.com>
2022-02-13 21:32:41 +0200

    Jelena: Lesson 4, Level 3

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Task10.java
@@ -0,0 +1,36 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+import java.util.Scanner;
+
+public class Task10 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter three numbers");
+        int a = 200;
+        int b = 50 ;
+        int c = 100;
+
+        if (a <= b && b < c) {
+            System.out.println(c);
+
+        } else if (a > b && a > c){
+            System.out.println(a);
+
+        } else if (a > b && b >= c){
+            System.out.println(a);
+
+        } else if (a <= c && c < b){
+            System.out.println(b);
+
+        } else if (a < c && b < a){
+            System.out.println(b);
+
+        } else if (a < b && c < b){
+            System.out.println(b);
+
+        } else if (a > c && b < c){
+            System.out.println(a);
+    }
+    }
+}
