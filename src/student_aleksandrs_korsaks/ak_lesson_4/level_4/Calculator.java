0c30085bf3ad37d929250da3382cbcce2d8709eb
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-13 13:18:39 +0200

    Aleksandrs K.: completed lesson 4, level 4, Task 11

---
+++ src/student_aleksandrs_korsaks/ak_lesson_4/level_4/Calculator.java
@@ -0,0 +1,20 @@
+package student_aleksandrs_korsaks.ak_lesson_4.level_4;
+
+public class Calculator {
+
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int mul(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public double div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
