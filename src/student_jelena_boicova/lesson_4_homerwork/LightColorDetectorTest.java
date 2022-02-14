20cfbe651ef7c35bc3974da7ee5caf580eb6f4d1
Jelena01 <jelenaboicova01@gmail.com>
2022-02-14 12:26:04 +0200

    Jelena: Lesson 4, Level 5

--- src/student_jelena_boicova/lesson_4_homerwork/LightColorDetectorTest.java
+++ src/student_jelena_boicova/lesson_4_homerwork/LightColorDetectorTest.java
@@ -6,6 +6,12 @@ public class LightColorDetectorTest {
         LightColorDetectorTest lightColorDetectorTest = new LightColorDetectorTest();
 
         lightColorDetectorTest.violetTest();
+        lightColorDetectorTest.blueTest();
+        lightColorDetectorTest.greenTest();
+        lightColorDetectorTest.yellowTest();
+        lightColorDetectorTest.orangeTest();
+        lightColorDetectorTest.redTest();
+        lightColorDetectorTest.invisibleLightTest();
     }
 
     public void violetTest() {
@@ -18,5 +24,65 @@ public class LightColorDetectorTest {
             System.out.println("Violet test = FAIL");
         }
     }
-
+    public void blueTest() {
+        String expectedResult = "Blue";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(460);
+        if (realResult == expectedResult) {
+            System.out.println("Blue test = OK");
+        } else {
+            System.out.println("Blue test = FAIL");
+        }
+    }
+    public void greenTest() {
+        String expectedResult = "Green";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(495);
+        if (realResult == expectedResult) {
+            System.out.println("Green test = OK");
+        } else {
+            System.out.println("Green test = FAIL");
+        }
+    }
+    public void yellowTest() {
+        String expectedResult = "Yellow";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(570);
+        if (realResult == expectedResult) {
+            System.out.println("Yellow test = OK");
+        } else {
+            System.out.println("Yellow test = FAIL");
+        }
+    }
+    public void orangeTest() {
+        String expectedResult = "Orange";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(600);
+        if (realResult == expectedResult) {
+            System.out.println("Orange test = OK");
+        } else {
+            System.out.println("Orange test = FAIL");
+        }
+    }
+    public void redTest() {
+        String expectedResult = "Red";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(700);
+        if (realResult == expectedResult) {
+            System.out.println("Red test = OK");
+        } else {
+            System.out.println("Red test = FAIL");
+        }
+    }
+    public void invisibleLightTest() {
+        String expectedResult = "Invisible Light";
+        LightColorDetector lightColorDetector = new LightColorDetector();
+        String realResult = lightColorDetector.detect(800);
+        if (realResult == expectedResult) {
+            System.out.println("Invisible Light test = OK");
+        } else {
+            System.out.println("Invisible Light test = FAIL");
+        }
+    }
 }
+
