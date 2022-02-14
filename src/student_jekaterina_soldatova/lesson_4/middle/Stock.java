18d5b2525c413f78a49f0340bf587e73f86bc067
Jekaterina <kate.soldatova93@gmail.com>
2022-02-14 22:27:09 +0200

    commit

---
+++ src/student_jekaterina_soldatova/lesson_4/middle/Stock.java
@@ -0,0 +1,25 @@
+package student_jekaterina_soldatova.lesson_4.middle;
+
+class Stock {
+    String company;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    Stock(String company, int currentPrice) {
+        this.company = company;
+        this.currentPrice = currentPrice;
+        minPrice = currentPrice;
+        maxPrice = currentPrice;
+    }
+
+    String getPriceInformation() {
+        return "Company = \"" + company + "\", Current Price = " + currentPrice + ", Min Price = " + minPrice + ", Max Price = " + maxPrice;
+    }
+
+    void updatePrice(int newPrice) {
+        if (newPrice > currentPrice) maxPrice = newPrice;
+        else if (newPrice < currentPrice) minPrice = newPrice;
+        currentPrice = newPrice;
+    }
+}
---
+++ src/student_jekaterina_soldatova/lesson_4/middle/StockTest.java
@@ -0,0 +1,4 @@
+package student_jekaterina_soldatova.lesson_4.middle;
+
+public class StockTest {
+}
