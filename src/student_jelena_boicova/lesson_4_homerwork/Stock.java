f54bd552153f2d5e1c622926534b4d1573b48f6d
Jelena01 <jelenaboicova01@gmail.com>
2022-02-15 22:51:10 +0200

    Jelena: Lesson 4, Level 6

---
+++ src/student_jelena_boicova/lesson_4_homerwork/Stock.java
@@ -0,0 +1,26 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+public class Stock {
+    String companyName;
+    int currentPrice;
+    int minPrice;
+    int maxPrice;
+
+    public Stock() {
+    }
+
+    public String getPriceInformation() {
+        return "Company:" + companyName + ", currentPrice =" + currentPrice + ", maxPrice=" + maxPrice +", minPrice=" + minPrice;
+    }
+    public void updatePrice(int newPrice){
+        currentPrice = newPrice;
+        if(newPrice < minPrice){
+            minPrice = newPrice;
+        }
+        else if(newPrice > maxPrice){
+            maxPrice = newPrice;
+        }
+
+    }
+
+}
