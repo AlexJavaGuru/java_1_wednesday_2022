37037fadb355e1ed288a62467414fd8a4f31049a
Jelena01 <jelenaboicova01@gmail.com>
2022-02-15 22:19:39 +0200

    Jelena: Lesson 4, Level 5

---
+++ src/student_jelena_boicova/lesson_4_homerwork/StockTest.java
@@ -0,0 +1,28 @@
+package student_jelena_boicova.lesson_4_homerwork;
+
+public class StockTest {
+
+    public static void main(String[] args) {
+
+        Stock google;
+        google.updatePrice(15);
+        google.updatePrice(7);
+        google.updatePrice(14);
+
+
+        StockTest stockTest = new StockTest();
+        priceInformation = google.getPriceInformation();
+        System.out.println(priceInformation);
+    }
+    public void updatePrice() {
+        int currentPrice = 14;
+        int minPrice = 7;
+        int maxPrice = 15;
+        int expectedResult = 14;
+        Stock google = new Stock();
+        int realResult = stock.updatePrice (currentPrice, minPrice, maxPrice);
+        if (realResult == expectedResult){
+
+    }
+}
+}
