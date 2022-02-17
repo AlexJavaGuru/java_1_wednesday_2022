d47b88f4ef0ef8718ec9cb7766b91cc3a2beb393
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-17 21:25:13 +0200

    Aleksandrs K.: completed lesson 5, level 2, Task 11

---
+++ src/student_aleksandrs_korsaks/ak_lesson_5/level_2/TaskEleven.java
@@ -0,0 +1,16 @@
+package student_aleksandrs_korsaks.ak_lesson_5.level_2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class TaskEleven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Please inter integer for array`s element nr. " + i);
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
