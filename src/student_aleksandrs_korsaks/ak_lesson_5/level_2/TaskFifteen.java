e7bd71c60b881fd1acb2a55bab1dcb3607fa8885
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-17 23:14:54 +0200

    Aleksandrs K.: completed lesson 5, level 2, Task 15

---
+++ src/student_aleksandrs_korsaks/ak_lesson_5/level_2/TaskFifteen.java
@@ -0,0 +1,17 @@
+package student_aleksandrs_korsaks.ak_lesson_5.level_2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class TaskFifteen {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[3];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(10);
+            System.out.println("Array`s element nr. " + (i + 1) + " = " + numbers[i]);
+            numbers[i] += 2;
+        }
+        System.out.println("Array`s element + 2 = " + Arrays.toString(numbers));
+    }
+}
