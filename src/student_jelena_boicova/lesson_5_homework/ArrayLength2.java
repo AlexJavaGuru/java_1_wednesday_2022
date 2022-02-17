e7dc9dab3f3a56f1bbf778f5dac72962e4cd9f11
Jelena01 <jelenaboicova01@gmail.com>
2022-02-17 11:10:42 +0200

    Jelena: Lesson 5, Task8

---
+++ src/student_jelena_boicova/lesson_5_homework/ArrayLength2.java
@@ -0,0 +1,11 @@
+package student_jelena_boicova.lesson_5_homework;
+
+class ArrayLength2 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[3];
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
