7814b6823eb12cf78da171992934edccc955e7a3
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-17 21:02:39 +0200

    Aleksandrs K.: completed lesson 5, level 2, Task 10

---
+++ src/student_aleksandrs_korsaks/ak_lesson_5/level_2/TaskTen.java
@@ -0,0 +1,10 @@
+package student_aleksandrs_korsaks.ak_lesson_5.level_2;
+
+import java.util.Arrays;
+
+class TaskTen {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
