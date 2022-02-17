013896fedfd6294c5b1ab47b1a2dbc877b7d42d2
Jelena01 <jelenaboicova01@gmail.com>
2022-02-17 11:03:24 +0200

    Jelena: Lesson 5, Task5

---
+++ src/student_jelena_boicova/lesson_5_homework/ArrayDefinition5.java
@@ -0,0 +1,8 @@
+package student_jelena_boicova.lesson_5_homework;
+
+class ArrayDefinition5 {
+
+        public static void main(String[] args) {
+            int [] numbers = {1, 2, 3, 4,5 };
+        }
+}
