971441963eb866fc6f094a1625a1ec773b139863
JaKo2577 <jako2577@gmail.com>
2022-02-17 19:39:28 +0200

    Level 1

---
+++ src/student_aleksandrs_jakovenko/lesson_5/level_1/ArrayLength1.java
@@ -0,0 +1,9 @@
+package student_aleksandrs_jakovenko.lesson_5.level_1;
+
+class ArrayLength1 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers[0]);
+    }
+}
