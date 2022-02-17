a25f3691ed4d34d1d35c1a1e87bebcdecef5d82c
JaKo2577 <jako2577@gmail.com>
2022-02-17 23:30:02 +0200

    Level 4

---
+++ src/student_aleksandrs_jakovenko/lesson_5/level_4/Task25.java
@@ -0,0 +1,21 @@
+package student_aleksandrs_jakovenko.lesson_5.level_4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+class Task25 {
+    public static void main(String[] args) {
+        int[] numbers;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array length:");
+        int size = scanner.nextInt();
+        numbers = new int[size];
+        System.out.println("Enter numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            int input = scanner.nextInt();
+            numbers[i] = input;
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ src/student_aleksandrs_jakovenko/lesson_5/level_4/Task27.java
@@ -0,0 +1,29 @@
+package student_aleksandrs_jakovenko.lesson_5.level_4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+class Task27 {
+    public static void main(String[] args) {
+        int[] numbers;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array length:");
+        int size = scanner.nextInt();
+        numbers = new int[size];
+        for (int i = 0; i < numbers.length; i++) {
+            Random random = new Random();
+            numbers[i] = random.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+
+        int max = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            max = numbers[0];
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Max number = " + max);
+    }
+}
