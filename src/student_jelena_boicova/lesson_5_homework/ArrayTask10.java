084322223d2cd1331e7d6154138d22028adcb856
Jelena01 <jelenaboicova01@gmail.com>
2022-02-17 11:29:10 +0200

    Jelena: Lesson 5, Task10

---
+++ src/student_jelena_boicova/lesson_5_homework/ArrayTask10.java
@@ -0,0 +1,16 @@
+package student_jelena_boicova.lesson_5_homework;
+
+import java.util.Arrays;
+
+class ArrayTask10 {
+
+    public static void main(String[] args) {
+        int [] numbers = new int[3];
+        numbers[0] = 100;
+        numbers[1] = 200;
+        numbers[2] = 300;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        System.out.println(numbers[2]);
+    }
+}
