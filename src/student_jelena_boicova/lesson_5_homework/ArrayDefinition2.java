a3ed88cc534fa390c25c9f1aadf52e1c25978f54
Jelena01 <jelenaboicova01@gmail.com>
2022-02-17 10:53:59 +0200

    Jelena: Lesson 5, Task2

--- src/student_jelena_boicova/lesson_5_homework/ArrayDefinition2.java
+++ src/student_jelena_boicova/lesson_5_homework/ArrayDefinition2.java
@@ -1,10 +1,8 @@
 package student_jelena_boicova.lesson_5_homework;
 
-class ArrayDefinition1 {
+public class ArrayDefinition2 {
 
     public static void main(String[] args) {
-        int[] numbers = new int[9];
+        byte [] numbers = new byte[7];
     }
-
 }
-
