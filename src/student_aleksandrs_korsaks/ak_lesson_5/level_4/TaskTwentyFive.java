a670c7a73c17b6a1ef5ee5235fbc1dc74a580ce8
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-02-18 18:25:33 +0200

    Aleksandrs K.: completed lesson 5, level 4, Task 25 + Task 11 fix grammar mistake

--- src/student_aleksandrs_korsaks/ak_lesson_5/level_4/TaskTwentyFive.java
+++ src/student_aleksandrs_korsaks/ak_lesson_5/level_4/TaskTwentyFive.java
@@ -1,12 +1,14 @@
-package student_aleksandrs_korsaks.ak_lesson_5.level_2;
+package student_aleksandrs_korsaks.ak_lesson_5.level_4;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-class TaskEleven {
+class TaskTwentyFive {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] numbers = new int[3];
+        System.out.println("Please enter the Array`s length");
+        int arrayLength = scanner.nextInt();
+        int[] numbers = new int[arrayLength];
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Please, enter integer for array`s element nr. " + (i + 1));
             numbers[i] = scanner.nextInt();
