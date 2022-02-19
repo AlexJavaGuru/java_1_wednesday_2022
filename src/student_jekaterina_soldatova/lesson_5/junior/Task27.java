059663628b736be3292a6aefffdf2ac011bfa670
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 19:46:36 +0200

    refactor27

--- src/student_jekaterina_soldatova/lesson_5/junior/Task27.java
+++ src/student_jekaterina_soldatova/lesson_5/junior/Task27.java
@@ -7,23 +7,19 @@
  */
 package student_jekaterina_soldatova.lesson_5.junior;
 
+import student_jekaterina_soldatova.lesson_5.middle.ArrayUtil;
+
 import java.util.Scanner;
 
 class Task27 {
     public static void main(String[] args) {
+        ArrayUtil arrayUtil = new ArrayUtil();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter length of the array");
         int arrayLength = scanner.nextInt();
         int[] numbers = new int[arrayLength];
-        int max = 0;
-
-        for (int i = 0; i < arrayLength; i++) {
-            numbers[i] = (int)(Math.random()*10);
-            System.out.println(numbers[i]);
-            if (max < numbers[i]) {
-                max = numbers[i];
-            }
-        }
-        System.out.println("Biggest number is " + max);
+        arrayUtil.fillArrayWithRandomNumbers(numbers);
+        arrayUtil.printArrayToConsole(numbers);
+        System.out.println("Biggest number is " + arrayUtil.findMaxNumber(numbers));
     }
 }
