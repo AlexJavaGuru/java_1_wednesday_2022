448666eb8232c1aa917fcba06e8ae74d8b4a4420
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 16:13:42 +0200

    task10

--- src/student_jekaterina_soldatova/lesson_5/intern/Task10.java
+++ src/student_jekaterina_soldatova/lesson_5/intern/Task10.java
@@ -10,7 +10,6 @@ package student_jekaterina_soldatova.lesson_5.intern;
 class Task10 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
-
         for (int i = 0; i < 3; i++) {
             numbers[i] = i;
             System.out.println(numbers[i]);
