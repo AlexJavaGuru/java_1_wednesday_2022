db6b2d69402cd614b696732cdd7ff0d0b25e7f49
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 15:48:57 +0200

    task1

---
+++ src/student_jekaterina_soldatova/lesson_5/Task1.java
@@ -0,0 +1,22 @@
+/*
+Найдите ошибку в программе и исправить её:
+
+class ArrayDefinition1 {
+
+    public static void main(String[] args) {
+        int numbers = new int[];
+    }
+
+}
+
+ */
+package student_jekaterina_soldatova.lesson_5;
+
+class Task1 {
+
+    public static void main(String[] args) {
+        int[] numbers;
+    }
+
+}
+
