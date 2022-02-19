b39f3157d703e91ff473d0d87b735c62c083224d
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 19:05:14 +0200

    ArrayUtilTest-minAndMax

--- src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtil.java
+++ src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtil.java
@@ -1,26 +1,43 @@
-/*
-Создайте класс ArrayUtil.
-Напишите в этом классе метод для создания
-массива целых чисел указанной длины.
-
-class ArrayUtil {
-
-    public int[] createArray(int arrayLength) {
-        // Напишите реализацию !!!
-    }
-
-}
- */
 package student_jekaterina_soldatova.lesson_5.middle;
 
 class ArrayUtil {
     int[] createArray(int arrayLength) {
-        return new int[arrayLength];
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            array[i] = i;
+        }
+        return array;
     }
 
     void fillArrayWithRandomNumbers(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10);
         }
+    }
+
+    void printArrayToConsole(int[] array) {
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    int findMaxNumber(int[] array) {
+        int max = 0;
+        for (int i: array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    int findMinNumber(int[] array) {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
     }
 }
--- src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtilTest.java
+++ src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtilTest.java
@@ -24,6 +24,8 @@ class ArrayUtilTest {
     public static void main(String[] args) {
         ArrayUtilTest test = new ArrayUtilTest();
         test.shouldCreateArray();
+        test.shouldFindMaxNumber();
+        test.shouldFindMinNumber();
     }
 
     void shouldCreateArray() {
@@ -35,6 +37,30 @@ class ArrayUtilTest {
             System.out.println("array length = OK");
         } else {
             System.out.println("array length = FAIL");
+        }
+    }
+
+    public void shouldFindMaxNumber() {
+        int expectedResult = 6;
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = arrayUtil.createArray(7);
+        int realResult = arrayUtil.findMaxNumber(array);
+        if (realResult == expectedResult) {
+            System.out.println("array max number = OK");
+        } else {
+            System.out.println("array max number = FAIL");
+        }
+    }
+
+    public void shouldFindMinNumber() {
+        int expectedResult = 0;
+        ArrayUtil arrayUtil = new ArrayUtil();
+        int[] array = arrayUtil.createArray(7);
+        int realResult = arrayUtil.findMinNumber(array);
+        if (realResult == expectedResult) {
+            System.out.println("array min number = OK");
+        } else {
+            System.out.println("array min number = FAIL");
         }
     }
 }
