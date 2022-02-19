eb01a60b851c6ab9d0120b71c5aa35a02261eeed
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 18:24:31 +0200

    ArrayUtil

---
+++ src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtil.java
@@ -0,0 +1,26 @@
+/*
+Создайте класс ArrayUtil.
+Напишите в этом классе метод для создания
+массива целых чисел указанной длины.
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        // Напишите реализацию !!!
+    }
+
+}
+ */
+package student_jekaterina_soldatova.lesson_5.middle;
+
+class ArrayUtil {
+    int[] createArray(int arrayLength) {
+        int[] numbers = new int[arrayLength];
+
+        for (int i = 0; i < arrayLength; i++) {
+            numbers[i] = (int) (Math.random() * 10);
+            System.out.println(numbers[i]);
+        }
+    return numbers;
+    }
+}
---
+++ src/student_jekaterina_soldatova/lesson_5/middle/ArrayUtilTest.java
@@ -0,0 +1,35 @@
+/*
+Создайте класс ArrayUtilTest.
+В этом классе напишите автоматический тест
+для метода int[] createArray(int arrayLength)
+из предыдущего задания.
+
+class ArrayUtilTest {
+
+    public static void main(String[] args) {
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+    }
+
+    public void shouldCreateArray() {
+        // Напишите реализацию !!!
+        // Проверьте, что метод создаёт массив указанной длины
+    }
+
+}
+ */
+package student_jekaterina_soldatova.lesson_5.middle;
+
+class ArrayUtilTest {
+        public static void main(String[] args) {
+            ArrayUtilTest test = new ArrayUtilTest();
+            test.shouldCreateArray();
+        }
+
+        public void shouldCreateArray() {
+            // Напишите реализацию !!!
+            // Проверьте, что метод создаёт массив указанной длины
+        }
+
+    }
+}
