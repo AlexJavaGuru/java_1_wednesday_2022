9e1c19fd7e51a1a1ca389bf2abfa34784b5d042a
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 20:02:04 +0200

    refactor

--- src/student_jekaterina_soldatova/lesson_5/intern/Task4.java
+++ src/student_jekaterina_soldatova/lesson_5/intern/Task4.java
@@ -14,10 +14,6 @@ package student_jekaterina_soldatova.lesson_5.intern;
 
 class Task4 {
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
+        int[] numbers = {1, 2, 3};
     }
 }
--- src/student_jekaterina_soldatova/lesson_5/intern/Task6.java
+++ src/student_jekaterina_soldatova/lesson_5/intern/Task6.java
@@ -13,10 +13,6 @@ package student_jekaterina_soldatova.lesson_5.intern;
 
 class Task6 {
     public static void main(String[] args) {
-        int[] numbers = new int[4];
-
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
+        int[] numbers = {1, 2, 3};
     }
 }
