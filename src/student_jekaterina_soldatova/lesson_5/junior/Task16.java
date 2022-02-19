966b991bb6e99c1a7192c54796626597cedcc63e
Jekaterina <kate.soldatova93@gmail.com>
2022-02-19 16:43:24 +0200

    task16

---
+++ src/student_jekaterina_soldatova/lesson_5/junior/Task16.java
@@ -0,0 +1,22 @@
+/*
+Найдите синтаксическую ошибку в программе и исправьте её:
+
+class ForLoopDefinition1 {
+
+    public static void main(String[] args) {
+        for (int i; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+ */
+package student_jekaterina_soldatova.lesson_5.junior;
+
+class Task16 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
