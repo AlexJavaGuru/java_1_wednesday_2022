e2e53e6566e643f1ac35125ffd14c142ed809964
Jelena01 <jelenaboicova01@gmail.com>
2022-02-20 21:03:36 +0200

    Jelena: Lesson 5, Task30

--- src/student_jelena_boicova/lesson_5_homework/Task30.java
+++ src/student_jelena_boicova/lesson_5_homework/Task30.java
@@ -1,5 +1,23 @@
 package student_jelena_boicova.lesson_5_homework;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Task30 {
 
+    public static void main(String[] args) {
+        int [] numbers = new int [27];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++){
+            numbers [i] = random.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                System.out.println(number);
+            }
+        }
+    }
 }
