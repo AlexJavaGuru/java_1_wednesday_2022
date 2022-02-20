043eaf26d09e81238007384cdfaa44c1838bc336
Jelena01 <jelenaboicova01@gmail.com>
2022-02-20 19:59:51 +0200

    Jelena: Lesson 5, Task17

--- src/student_jelena_boicova/lesson_5_homework/ForLoopDefinition2.java
+++ src/student_jelena_boicova/lesson_5_homework/ForLoopDefinition2.java
@@ -1,6 +1,6 @@
 package student_jelena_boicova.lesson_5_homework;
 
-public class ForLoopDefinition1 {
+public class ForLoopDefinition2 {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
@@ -9,3 +9,4 @@ public class ForLoopDefinition1 {
     }
 
 }
+
