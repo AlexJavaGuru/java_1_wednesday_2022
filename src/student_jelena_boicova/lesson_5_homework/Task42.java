f399c5494e8a5d0320912219a2e03b6ff42dab2e
Jelena01 <jelenaboicova01@gmail.com>
2022-02-21 00:23:34 +0200

    Jelena: Lesson 5, Task42

--- src/student_jelena_boicova/lesson_5_homework/Task42.java
+++
@@ -1,5 +0,0 @@
-package student_jelena_boicova.lesson_5_homework;
-
-public class Task42 {
-
-}
---
+++ src/student_jelena_boicova/lesson_5_homework/Test.java
@@ -0,0 +1,14 @@
+package student_jelena_boicova.lesson_5_homework;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+            int[] array = {51,136,387};
+
+            for (int i:array) {
+                System.out.println(i);
+            }
+        }
+
+}
