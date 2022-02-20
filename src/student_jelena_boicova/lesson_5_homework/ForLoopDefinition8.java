eb2d83ffc91eafe4403d4d0f4ef1c9427f72860e
Jelena01 <jelenaboicova01@gmail.com>
2022-02-20 20:18:29 +0200

    Jelena: Lesson 5, Task24

---
+++ src/student_jelena_boicova/lesson_5_homework/ForLoopDefinition8.java
@@ -0,0 +1,10 @@
+package student_jelena_boicova.lesson_5_homework;
+
+public class ForLoopDefinition8 {
+    public static void main(String[] args) {
+        for ( int i = 0; i < 101; i++ ) {
+            System.out.println(i);
+        }
+    }
+
+}
