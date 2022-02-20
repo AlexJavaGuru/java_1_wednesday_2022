306bbd5b7d523ed7ad00d9f39d22c71c3230da9d
Jelena01 <jelenaboicova01@gmail.com>
2022-02-20 20:39:47 +0200

    Jelena: Lesson 5, Task26

---
+++ src/student_jelena_boicova/lesson_5_homework/Task26.java
@@ -0,0 +1,21 @@
+package student_jelena_boicova.lesson_5_homework;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task26 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter arrays length");
+        Scanner scanner = new Scanner(System.in);
+        int [] numbers = new int [scanner.nextInt()];
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++){
+            numbers [i] = random.nextInt();
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+
+}
