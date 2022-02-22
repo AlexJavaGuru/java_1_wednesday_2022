139eee4ca7530536d6d9233012e26e5398372fb7
Jelena01 <jelenaboicova01@gmail.com>
2022-02-22 12:36:12 +0200

    JB, Task "Dog" case

--- src/student_jelena_boicova/lesson_3/Dog.java
+++ src/student_jelena_boicova/lesson_3/Dog.java
@@ -2,11 +2,12 @@ package student_jelena_boicova.lesson_3;
 
 public class Dog {
 
-    String nickname = "Max";
-    int age = 15;
+    String nickname;
+    int age;
 
     public Dog(String nickname, int age){
-
+        this.nickname = nickname;
+        this.age = age;
     }
 
     public void setAge(int age) {
@@ -15,15 +16,13 @@ public class Dog {
 
     public void setNickname(String nickname) {
         this.nickname = nickname;
-
     }
+
     public String getNickname() {
         return nickname;
-
     }
 
     public void voice(){
         System.out.println(nickname + nickname + nickname + age);
-
     }
 }
