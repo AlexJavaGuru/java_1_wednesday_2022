1eb74719c09f06f852cf01bf08ef8c80e9f381b2
Jekaterina <kate.soldatova93@gmail.com>
2022-02-24 19:42:53 +0200

    task5

---
+++ src/student_jekaterina_soldatova/lesson06/level01/Task_5.java
@@ -0,0 +1,12 @@
+package student_jekaterina_soldatova.lesson06.level01;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
