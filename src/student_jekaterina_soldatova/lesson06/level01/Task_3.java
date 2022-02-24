9c2ab6e0cca5b28696e9f643e0ad21479a6f123c
Jekaterina <kate.soldatova93@gmail.com>
2022-02-24 19:39:45 +0200

    task3

--- src/student_jekaterina_soldatova/lesson06/level01/Task_3.java
+++ src/student_jekaterina_soldatova/lesson06/level01/Task_3.java
@@ -1,10 +1,9 @@
-Найдите ошибку в программе и исправить её:
+package student_jekaterina_soldatova.lesson06.level01;
 
-class WhileDefinition2 {
-
+public class Task_3 {
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 100)
+        while (i <= 100) {
             System.out.println(i);
             i++;
         }
