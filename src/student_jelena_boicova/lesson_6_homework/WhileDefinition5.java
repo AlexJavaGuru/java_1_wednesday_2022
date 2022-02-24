1ff2ad29f24141e14e7a6d45a1cc963d5c71dba0
Jelena01 <jelenaboicova01@gmail.com>
2022-02-24 17:58:37 +0200

    JB, Lesson 6, Task 5

---
+++ src/student_jelena_boicova/lesson_6_homework/WhileDefinition5.java
@@ -0,0 +1,13 @@
+package student_jelena_boicova.lesson_6_homework;
+
+public class WhileDefinition5 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+}
