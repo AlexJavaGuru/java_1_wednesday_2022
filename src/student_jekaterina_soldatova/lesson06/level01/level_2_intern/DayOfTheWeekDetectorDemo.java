5e515af2b49d10bd7fd43b0a53ed1d0ccf8bfa50
Jekaterina <kate.soldatova93@gmail.com>
2022-02-24 22:17:24 +0200

    day of the week

---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_2_intern/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,10 @@
+package student_jekaterina_soldatova.lesson06.level01.level_2_intern;
+
+class DayOfTheWeekDetectorDemo {
+    public static void main(String[] args) {
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        int dayNumber = detector.getDayNumberFromUser();
+        String dayOfTheWeek = detector.findDayOfTheWeek(dayNumber);
+        System.out.println(dayOfTheWeek);
+    }
+}
