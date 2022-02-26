e8021143126c8b8e209dda7567c779ba138078b9
JaKo2577 <jako2577@gmail.com>
2022-02-26 13:33:13 +0200

    Теперь работает, но с рандомными числами сложно проверить, т.к. выдаёт avg по типу -5.15435857E8. Пришлось ограничить границы рандома для теста.

--- src/student_aleksandrs_jakovenko/lesson_5/level_2/Task14.java
+++ src/student_aleksandrs_jakovenko/lesson_5/level_2/Task14.java
@@ -1,16 +1,20 @@
 package student_aleksandrs_jakovenko.lesson_5.level_2;
 
+import java.util.Arrays;
 import java.util.Random;
 
 class Task14 {
     public static void main(String[] args) {
         int[] numbers = new int[3];
-        int avg = 0;
+        double avg = 0;
+        int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             Random random = new Random();
             numbers[i] = random.nextInt();
-            avg = numbers[i]/ numbers.length;
+            sum += numbers[i];
+            avg = (double) sum / numbers.length;
         }
+        System.out.println(Arrays.toString(numbers));
         System.out.println(avg);
     }
 }
