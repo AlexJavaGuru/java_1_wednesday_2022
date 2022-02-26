f8551d6de693c00ce6a12126400242e6e9842d75
JaKo <jako2577@gmail.com>
2022-02-26 16:56:11 +0200

    Level 4

---
+++ src/student_aleksandrs_jakovenko/lesson_6/level_4/ForInfiniteLoop.java
@@ -0,0 +1,10 @@
+package student_aleksandrs_jakovenko.lesson_6.level_4;
+
+class ForInfiniteLoop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
