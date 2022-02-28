ad3ce7661d56efdbeb96737b7d793893436d8691
student_igors <kork2@inbox.lv>
2022-02-28 06:59:20 +0200

    TaskTwentyThree

---
+++ src/student_igors/Level_6/TaskTwentyFour.java
@@ -0,0 +1,9 @@
+package student_igors.Level_6;
+
+public class TaskTwentyFour {
+    public static void main(String[] args) {
+        System.out.println( "I wanna coffee" );
+        System.out.println("So bad");
+    }
+}
+
