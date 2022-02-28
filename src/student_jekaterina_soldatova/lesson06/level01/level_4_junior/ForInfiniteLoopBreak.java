3838945e47f441148558d3d9ab33484dd5e41e6f
Jekaterina <kate.soldatova93@gmail.com>
2022-02-28 21:31:14 +0200

    infinite loops w break

---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/ForInfiniteLoopBreak.java
@@ -0,0 +1,12 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+public class ForInfiniteLoopBreak {
+    public static void main(String[] args) {
+        for (int i = 1; i > 0; i++) {
+            System.out.println("i = " + i);
+            if (i == 5) {
+                break;
+            }
+        }
+    }
+}
---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/WhileInfiniteLoopBreak.java
@@ -0,0 +1,14 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+public class WhileInfiniteLoopBreak {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i > 0) {
+            System.out.println("i = " + i);
+            if (i == 5) {
+                break;
+            }
+            i++;
+        }
+    }
+}
