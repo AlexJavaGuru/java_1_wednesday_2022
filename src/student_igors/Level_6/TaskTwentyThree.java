c34fb93eb51f6bc6aa4f932693ae74c3551c7238
student_igors <kork2@inbox.lv>
2022-02-28 06:55:49 +0200

    TaskTwentyThree

---
+++ src/student_igors/Level_6/TaskTwentyThree.java
@@ -0,0 +1,9 @@
+package student_igors.Level_6;
+
+public class TaskTwentyThree {
+    public static void main(String[] args) {
+    System.out.println("Code outside the method will not compile!");
+
+    }
+}
+
