bb0057f99bba94ee3b57fa5fa566a28f5fabdca8
Jekaterina <kate.soldatova93@gmail.com>
2022-02-28 21:21:56 +0200

    infinite loops

---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/ForInfiniteLoop.java
@@ -0,0 +1,9 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+class ForInfiniteLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i > 0 ; i++) {
+            System.out.println("I am infinite");
+        }
+    }
+}
---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/WhileInfiniteLoop.java
@@ -0,0 +1,9 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+class WhileInfiniteLoop {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("I am infinite");
+        }
+    }
+}
