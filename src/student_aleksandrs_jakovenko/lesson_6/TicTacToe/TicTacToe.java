4ee4aa03e466df3dc4587381d6ceaf09d51f96c0
JaKo <jako2577@gmail.com>
2022-02-28 21:23:11 +0200

    update

---
+++ src/student_aleksandrs_jakovenko/lesson_6/TicTacToe/TicTacToe.java
@@ -0,0 +1,17 @@
+package student_aleksandrs_jakovenko.lesson_6.TicTacToe;
+
+class TicTacToe {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+
+            }
+        }
+        return false;
+    }
+}
