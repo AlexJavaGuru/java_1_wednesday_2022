ac3082cccf2eb094de227c416d18da1a8218908e
Jekaterina <kate.soldatova93@gmail.com>
2022-02-28 22:14:37 +0200

    twoDimensionalArray

---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/TwoDimensionalArray.java
@@ -0,0 +1,25 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+import java.util.Arrays;
+
+public class TwoDimensionalArray {
+    public static void main(String[] args) {
+        TwoDimensionalArray fill = new TwoDimensionalArray();
+
+        int[][] twoDimensionalArray = new int[fill.random()][fill.random()];
+        int sum = 0;
+
+        for (int i = 0; i < twoDimensionalArray.length; i++) {
+            for (int j = 0; j < twoDimensionalArray[0].length; j++) {
+                twoDimensionalArray[i][j] = fill.random();
+                System.out.print(twoDimensionalArray[i][j] + " ");
+                sum = sum + twoDimensionalArray[i][j];
+            }
+            System.out.println();
+        }
+    }
+
+    int random() {
+        return (int) (Math.random() * 10);
+    }
+}
