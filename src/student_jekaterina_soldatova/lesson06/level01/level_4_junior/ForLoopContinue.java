1baed01b1366da79ab99858a70e382d5d810b5a8
Jekaterina <kate.soldatova93@gmail.com>
2022-02-28 21:41:03 +0200

    infinite loops w continue

---
+++ src/student_jekaterina_soldatova/lesson06/level01/level_4_junior/ForLoopContinue.java
@@ -0,0 +1,13 @@
+package student_jekaterina_soldatova.lesson06.level01.level_4_junior;
+
+public class ForLoopContinue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            if (i == 3) {
+                System.out.println("continue is here");
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
