6df9d181cec7aee0d68a12a7d363f69c2fc7d0d0
ArtursMel <melnikov270@gmail.com>
2022-03-03 05:01:38 +0200

    ArturM: Lesson_06_Level_06 task 15

--- src/student_arturs_melnikovs/lesson_07/level_07/UserEntity.java
+++ src/student_arturs_melnikovs/lesson_07/level_07/UserEntity.java
@@ -1,6 +1,6 @@
 package student_arturs_melnikovs.lesson_07.level_07;
 
-public class UserEntity {
+class UserEntity {
     private int id;
     private String firstName;
     private String lastName;
