28995c228029421437edf0568782c49cdf8fa0ab
ArtursMel <melnikov270@gmail.com>
2022-03-03 05:01:24 +0200

    ArturM: Lesson_06_Level_06 task 15

---
+++ src/student_arturs_melnikovs/lesson_07/level_07/UserEntity.java
@@ -0,0 +1,8 @@
+package student_arturs_melnikovs.lesson_07.level_07;
+
+public class UserEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String personalCode;
+}
