4bdaa797d134baf4d6a72c11a67d8deeb8328358
student_igors <kork2@inbox.lv>
2022-03-05 16:10:52 +0200

    RobotDemo

---
+++ src/student_igors/lesson_3/RobotDemo.java
@@ -0,0 +1,32 @@
+package student_igors.lesson_3;
+
+import student_nikita_visnakov.nv_lesson2.Hello;
+
+import java.util.Scanner;
+
+public record RobotDemo() {
+
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+
+
+        Robot RobotDemo = new Robot("ROBO");
+        RobotDemo.sayHello("Hello!");
+        RobotDemo.sayYourName("ROBO");
+
+        Robot robotOne = new Robot("GARRY");
+        robotOne.sayHello("Hello!");
+        robotOne.sayYourName("GARRY");
+
+        Robot robotTwo = new Robot("FELIX");
+        robotTwo.sayHello("Hello!");
+        robotTwo.sayYourName("FELIX");
+
+
+        // создайте экземпляр класса Robot вызвав конструктор класса
+        // сохраните ссылку (указатель) на только, что созданный
+        // виртуальный обьект класса Robot в переменную,
+        // она нам понадобиться для дальнейшего использования.
+    }
+
+}
