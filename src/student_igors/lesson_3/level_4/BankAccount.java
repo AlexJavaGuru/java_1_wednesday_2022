1619f34cfdf0b0d86f0f9e6e407209e5858ed53c
student_igors <kork2@inbox.lv>
2022-03-06 15:04:33 +0200

    BankAccount

---
+++ src/student_igors/lesson_3/level_4/BankAccount.java
@@ -0,0 +1,29 @@
+package student_igors.lesson_3.level_4;
+
+public class BankAccount {
+
+          String ownerFirstName;
+        String ownerLastName;
+        int moneyAmount;
+
+        BankAccount(String ownerFirstName,
+                    String ownerLastName,
+                int moneyAmount) {
+            this.ownerFirstName = ownerFirstName;
+            this.ownerLastName = ownerLastName;
+            this.moneyAmount = moneyAmount;
+        }
+
+        String getOwnerFirstName(){
+            return this.ownerFirstName;
+        }
+
+        String getOwnerLastName() {
+            return this.ownerLastName;
+        }
+
+        int getMoneyAmount() {
+            return this.moneyAmount;
+        }
+
+    }
