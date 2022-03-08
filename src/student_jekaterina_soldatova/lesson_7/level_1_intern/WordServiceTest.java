0bc213231f144da384eb58052e5ff5e2a702e843
Jekaterina <kate.soldatova93@gmail.com>
2022-03-08 21:41:13 +0200

    power calc

--- src/student_jekaterina_soldatova/lesson_7/level_1_intern/WordServiceTest.java
+++ src/student_jekaterina_soldatova/lesson_7/level_1_intern/WordServiceTest.java
@@ -10,7 +10,7 @@ class WordServiceTest {
 
     void stringToArray() {
         String[] expectedResult = {"Huh", "Joe", "waited", "for", "the", "train", "the", "train", "was", "late"};
-        String[] realResult = WordService.stringToArray("Huh, Joe waited for the train, the train was late!");
+        String[] realResult = WordService.stringToArray("Huh - Joe waited for the train, the train was late!");
         if (compareArrays(expectedResult, realResult)) {
             System.out.println("string to array = OK");
         } else {
---
+++ src/student_jekaterina_soldatova/lesson_7/level_5_middle/PowerCalculatorTest.java
@@ -0,0 +1,9 @@
+package student_jekaterina_soldatova.lesson_7.level_5_middle;
+
+class PowerCalculatorTest {
+    public static void main(String[] args) {
+        PowerCalculatorTest test = new PowerCalculatorTest();
+
+
+    }
+}
