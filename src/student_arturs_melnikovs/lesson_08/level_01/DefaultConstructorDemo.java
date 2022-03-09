36c070862d5fe68437fb02670932b7b73cb556b0
ArtursMel <melnikov270@gmail.com>
2022-03-09 21:50:41 +0200

    ArturM: Lesson_08_Level_01

---
+++ src/student_arturs_melnikovs/lesson_08/level_01/DefaultConstructorDemo.java
@@ -0,0 +1,7 @@
+package student_arturs_melnikovs.lesson_08.level_01;
+
+class DefaultConstructorDemo {
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
---
+++ src/student_arturs_melnikovs/lesson_08/level_01/DefaultConstructorDemoV2.java
@@ -0,0 +1,7 @@
+package student_arturs_melnikovs.lesson_08.level_01;
+
+public class DefaultConstructorDemoV2 {
+    public static void main(String[] args) {
+        DefaultConstructorV2 defaultConstructorV2 = new DefaultConstructorV2();
+    }
+}
