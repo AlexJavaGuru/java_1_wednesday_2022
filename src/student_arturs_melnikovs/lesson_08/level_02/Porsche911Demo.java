6b71e90443762c72cc87c7059c64b391334d2c6e
ArtursMel <melnikov270@gmail.com>
2022-03-09 21:57:59 +0200

    ArturM: Lesson_08_Level_02

---
+++ src/student_arturs_melnikovs/lesson_08/level_02/Porsche911Demo.java
@@ -0,0 +1,7 @@
+package student_arturs_melnikovs.lesson_08.level_02;
+
+class Porsche911Demo {
+    public static void main(String[] args) {
+        Porsche911 porsche911 = new Porsche911(2020);
+    }
+}
