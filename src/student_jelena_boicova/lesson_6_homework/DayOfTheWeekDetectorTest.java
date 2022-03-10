0074a695f9e50ea192eb16f5884138c59aa8aeef
Jelena01 <jelenaboicova01@gmail.com>
2022-03-10 09:54:57 +0200

    JB, Lesson 6, Task 7-11

--- src/student_jelena_boicova/lesson_6_homework/DayOfTheWeekDetectorTest.java
+++ src/student_jelena_boicova/lesson_6_homework/DayOfTheWeekDetectorTest.java
@@ -5,6 +5,7 @@ public class DayOfTheWeekDetectorTest {
     public static void main(String[] args) {
         DayOfTheWeekDetectorTest test = new DayOfTheWeekDetectorTest();
         test.shouldReturnMonday();
+        test.shouldReturnTuesday();
     }
 
     public void shouldReturnMonday(){
@@ -15,6 +16,17 @@ public class DayOfTheWeekDetectorTest {
             System.out.println ("Monday test = OK");
         } else {
             System.out.println ("Monday test = FAIL");
+        }
+    }
+
+    public void shouldReturnTuesday(){
+        DayOfTheWeekDetector detector = new DayOfTheWeekDetector();
+        String actualResult = detector.findDayOfTheWeek(2);
+        String expectedResult = "Tuesday";
+        if (actualResult == expectedResult){
+            System.out.println ("Tuesday test = OK");
+        } else {
+            System.out.println ("Tuesday test = FAIL");
         }
     }
 }
