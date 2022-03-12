ede77378b3c5080d8e166dc761ad83c3040a9978
student_igors <kork2@inbox.lv>
2022-03-12 17:55:35 +0200

    ForloopDefinition9

---
+++ src/student_igors/lesson_5/level_3/ForloopDefinition9.java
@@ -0,0 +1,12 @@
+package student_igors.lesson_5.level_3;
+
+public class ForloopDefinition9 {
+     public static void main(String[] args) {
+    for (int i = 0; i < 101; i++) {
+        System.out.println(i);
+    }
+ }
+}
+
+//  Исправьте код таким образом, что бы программа
+//  выводила на консоль числа от 0 до 100:
