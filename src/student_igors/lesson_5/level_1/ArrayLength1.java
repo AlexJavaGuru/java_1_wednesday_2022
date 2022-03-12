76c8c5050e55fa48bbfe77a25b868ce0e098cada
student_igors <kork2@inbox.lv>
2022-03-12 10:25:36 +0200

    ArrayLength1

---
+++ src/student_igors/lesson_5/level_1/ArrayLength1.java
@@ -0,0 +1,12 @@
+package student_igors.lesson_5.level_1;
+
+public class ArrayLength1 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        numbers[0] = 1;
+        numbers[1] = 2;
+            System.out.println(numbers[1]);
+    }
+
+}
+
