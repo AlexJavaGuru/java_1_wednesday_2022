8a255577389b63d8b8d359e14168d43edf80e8bb
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-03-12 18:39:19 +0200

    Aleksandrs K.: lesson 7, level 1, Task 1-3

--- src/student_aleksandrs_korsaks/ak_lesson_7/level_1/WordService.java
+++ src/student_aleksandrs_korsaks/ak_lesson_7/level_1/WordService.java
@@ -1,84 +1,48 @@
 package student_aleksandrs_korsaks.ak_lesson_7.level_1;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 class WordService {
-
     public static void main(String[] args) {
 
         WordService wordService = new WordService();
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter the text:");
         String text = scanner.nextLine();
         scanner.close();
 
-        System.out.println(wordService.findMostFrequentWord(text));
+        String mostFrequentWord = wordService.findMostFrequentWord(text);
+        System.out.println("The most frequent word in the text is - \"" + mostFrequentWord + "\"");
+    }
 
+    public String filterTextFromUnnecessaryCharactersPutThemInLowerCase(String text) {
+        return text.replaceAll("[^a-zA-Z\s]", " ").toLowerCase();
+    }
+
+    public String[] separateTextToWords(String text) {
+        return filterTextFromUnnecessaryCharactersPutThemInLowerCase(text).split("\s+");
     }
 
     public String findMostFrequentWord(String text) {
-
-        String[] filteredWords = filterAndSeparateTextToWords(text);
-        int[] quantityOfWordsRepetitions = countRepeatedWordsQuantity(filteredWords);
-        System.out.println(Arrays.toString(quantityOfWordsRepetitions));
-        return findMostRepeatedWord(filteredWords, quantityOfWordsRepetitions);
-
-    }
-
-    public String[] filterAndSeparateTextToWords(String text) {
-
-        String[] words = text.replaceAll("[^a-z^A-Z^\s]", "").toLowerCase().split(" ");
-        int filteredWordsArraySize = 0;
+        String[] words = separateTextToWords(text);
+        int maxNumberOfWordRepetitions = 0;
+        String mostFrequentWord = "";
 
         for (String word : words) {
-            if (!word.isEmpty()) {
-                filteredWordsArraySize++;
-            }
-        }
+            int numberOfWordRepetitions = 0;
 
-        String[] filteredWords = new String[filteredWordsArraySize];
-        int k = 0;
-        int i = 0;
-        while (i < words.length) {
-            if (!words[i].isEmpty()) {
-                filteredWords[k] = words[i];
-                i++;
-                k++;
-            } else {
-                i++;
-            }
-        }
-        return filteredWords;
-
-    }
-
-    public int[] countRepeatedWordsQuantity(String[] words) {
-
-        int[] quantityOfWordsRepetitions = new int[words.length];
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    quantityOfWordsRepetitions[i]++;
+            for (String wordToCompare : words) {
+                if (word.equals(wordToCompare)) {
+                    numberOfWordRepetitions++;
                 }
             }
-        }
-        return quantityOfWordsRepetitions;
 
-    }
-
-    private String findMostRepeatedWord(String[] words, int[] counter) {
-
-        int max = counter[0];
-        int indexOfMostRepeatedWord = 0;
-        for (int i = 0; i < counter.length; i++) {
-            if (counter[i] > max) {
-                max = counter[i];
-                indexOfMostRepeatedWord = i;
+            if (numberOfWordRepetitions > maxNumberOfWordRepetitions) {
+                maxNumberOfWordRepetitions = numberOfWordRepetitions;
+                mostFrequentWord = word;
             }
         }
-        return words[indexOfMostRepeatedWord];
-
+        return mostFrequentWord;
     }
 }
+
