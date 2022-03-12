1e03558b86dfafd16eaf95c5500969e17efab9a4
student_igors <kork2@inbox.lv>
2022-03-12 10:56:08 +0200

    TaskTen

---
+++ src/student_igors/lesson_5/level_2/TaskTen.java
@@ -0,0 +1,12 @@
+package student_igors.lesson_5.level_2;
+
+public class TaskTen {
+    public static void main(String[] args) {
+        int[] number =new int[3];
+        number[0] = 5;
+        number[1] = 10;
+        number[2] = 15;
+        for (int i = 0; i < number.length; i++)
+        System.out.println("[" + i + "]: " + number[i]);
+    }
+}
