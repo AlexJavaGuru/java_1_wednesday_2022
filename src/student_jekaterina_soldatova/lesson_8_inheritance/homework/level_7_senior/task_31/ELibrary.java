8e0a5553f06d5f5a7aba0510b2e4a7577d4d3f75
Jekaterina <kate.soldatova93@gmail.com>
2022-03-13 23:14:08 +0200

    task31

--- src/student_jekaterina_soldatova/lesson_8_inheritance/homework/level_7_senior/task_31/ELibrary.java
+++ src/student_jekaterina_soldatova/lesson_8_inheritance/homework/level_7_senior/task_31/ELibrary.java
@@ -1,17 +1,13 @@
 package student_jekaterina_soldatova.lesson_8_inheritance.homework.level_7_senior.task_31;
 
 class ELibrary {
-    //- взять книгу из библиотеки;
-    //- вернуть книгу в библиотеку;
-    //- поиск нужной книги;
-    //- резервация книги;
+    //- взять книгу из библиотеки; +
+    //- вернуть книгу в библиотеку; +
+    //- поиск нужной книги; +
+    //- резервация книги; +
     //- рассылка оповещений читателям о сроке сдачи книги в библиотеку;
     //- выписывание штрафов за вовремя не сданные книги;
-    //- добавление новой книги в библиотеку.
-
-    //array of books: author, title, count of books, array[clients who took], array[clients who reserved]
-    //array of clients: name, array[book taken, days taken], array[reserved books]
-    //take book (array of books: count of books-1, array[clients who took]: +name OR array[clients who reserved]: +name)(array of clients:
+    //- добавление новой книги в библиотеку. +
 
     void takeBook(Book thisBook, Client byClient) {
         thisBook.takeBook(byClient);
