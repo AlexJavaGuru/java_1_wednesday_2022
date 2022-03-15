ebf6a1aaa3217edf409118b2527d7c04bdc6d2a9
JaKo2577 <jako2577@gmail.com>
2022-03-15 02:09:15 +0200

    Level 2

---
+++ src/student_aleksandrs_jakovenko/lesson_8/level_2/ComputerPlayerDemo.java
@@ -0,0 +1,8 @@
+package student_aleksandrs_jakovenko.lesson_8.level_2;
+
+class ComputerPlayerDemo {
+
+    public static void main(String[] args) {
+        ComputerPlayer computerPlayer = new ComputerPlayer("Alex");
+    }
+}
