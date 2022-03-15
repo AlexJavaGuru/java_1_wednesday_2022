55bbadf666547d5b22d621745dd3e8e0dd50ddcd
Jelena01 <jelenaboicova01@gmail.com>
2022-03-15 23:37:01 +0200

    JB, Lesson 6, Task 13-15

--- src/student_jelena_boicova/lesson_6_homework/ArrayService.java
+++ src/student_jelena_boicova/lesson_6_homework/ArrayService.java
@@ -10,4 +10,35 @@ public class ArrayService {
         }
         return false;
     }
+
+    int countOccurrences(int[] array, int numberToSearch){
+        int number = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == numberToSearch){
+                number++;
+        }
+    }
+        return number;
+    }
+
+    boolean replaceFirst(int[] array, int numberToReplace, int newNumber){
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == numberToReplace){
+                array[i] = newNumber;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int replaceAll(int[] array, int numberToReplace, int newNumber){
+        int replace = 0;
+        for (int i = 0; i < array.length; i++){
+            if (array[i] == numberToReplace){
+                array[i] = newNumber;
+                replace++;
+            }
+        }
+        return replace;
+    }
 }
--- src/student_jelena_boicova/lesson_6_homework/ArrayServiceTest.java
+++ src/student_jelena_boicova/lesson_6_homework/ArrayServiceTest.java
@@ -4,17 +4,108 @@ public class ArrayServiceTest {
 
     public static void main(String[] args){
         ArrayServiceTest test = new ArrayServiceTest();
-        test.searchNumber();
+        test.testSearchNumber();
+        test.testNotSearchNumber();
+        test.testCountOccurrences();
+        test.testCountOccurrencesNegative();
+        test.testReplaceFirst();
+        test.testNotReplaceFirst();
+        test.testReplaceAll();
+        test.testNotReplaceAll();
+
     }
 
-    public void searchNumber(){
+    public void testSearchNumber(){
         ArrayService test = new ArrayService();
         int[] array = {1, 2, 3, 4, 5};
         boolean actualResult = test.searchNumber(array,4);
         if (actualResult){
-            System.out.println("Searched number is found");
+            System.out.println("Searched number is found in Massive");
         } else {
-            System.out.println("Searched number is not found");
+            System.out.println("Searched number is not found in Massive");
+        }
+    }
+
+    public void testNotSearchNumber(){
+        ArrayService test = new ArrayService();
+        int[] array = {1, 2, 3, 4, 5};
+        boolean actualResult = test.searchNumber(array,37);
+        if (actualResult){
+            System.out.println("Searched number is found in Massive");
+        } else {
+            System.out.println("Searched number is not found in Massive");
+        }
+    }
+
+    public void testCountOccurrences(){
+        ArrayService test = new ArrayService();
+        int[] array = {4, 8, 7, 5, 7};
+        int expectedResult = 7;
+        int actualResult = test.countOccurrences(array, 7);
+        if (expectedResult == actualResult){
+            System.out.println(expectedResult + "Occurrences OK");
+        } else {
+            System.out.println(expectedResult + "Occurrences FALSE");
+        }
+    }
+
+    public void testCountOccurrencesNegative(){
+        ArrayService test = new ArrayService();
+        int[] array = {4, 8, 7, 5, 7};
+        int expectedResult = 3;
+        int actualResult = test.countOccurrences(array,3);
+        if (expectedResult == actualResult){
+            System.out.println(expectedResult + "Occurrences OK");
+        } else {
+            System.out.println(expectedResult + "Occurrences FALSE");
+        }
+    }
+
+    public void testReplaceFirst(){
+        ArrayService test = new ArrayService();
+        int[] array = {2, 2, 3, 4, 5};
+        int expectedResult = 2;
+        boolean actualResult = test.replaceFirst(array, 2,1);
+        if (actualResult){
+            System.out.println(expectedResult + "First number replaced");
+        } else {
+            System.out.println(expectedResult + "First number not replaced");
+        }
+     }
+
+     public void testNotReplaceFirst(){
+         ArrayService test = new ArrayService();
+         int[] array = {2, 2, 3, 4, 5};
+         int expectedResult = 6;
+         boolean actualResult = test.replaceFirst(array, 6,1);
+         if (actualResult){
+             System.out.println(expectedResult + "First number replaced");
+         } else {
+             System.out.println(expectedResult + "First number not replaced");
+         }
+     }
+
+     public void testReplaceAll(){
+         ArrayService test = new ArrayService();
+         int[] array = {1, 2, 3, 2, 5};
+         int expectedResult = 2;
+         int actualResult = test.replaceAll(array, 2, 4 );
+         if (expectedResult == actualResult){
+             System.out.println(expectedResult + "All numbers replaced");
+         } else {
+             System.out.println(expectedResult + "All numbers not replaced");
+         }
+     }
+
+     public void testNotReplaceAll(){
+         ArrayService test = new ArrayService();
+         int[] array = {10, 20, 30, 20, 20};
+         int expectedResult = 5;
+         int actualResult = test.replaceAll(array, 5, 4);
+         if (expectedResult == actualResult){
+         System.out.println(expectedResult + "All numbers replaced");
+        } else {
+        System.out.println(expectedResult + "All numbers not replaced");
         }
     }
 }
