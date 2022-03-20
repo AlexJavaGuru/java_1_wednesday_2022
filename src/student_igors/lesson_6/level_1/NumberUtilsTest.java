8bcfba207751df497bed5102e46f6908502e5c4c
student_igors <kork2@inbox.lv>
2022-03-20 08:46:39 +0200

    NumberUtils

---
+++ src/student_igors/lesson_6/level_1/NumberUtilsTest.java
@@ -0,0 +1,9 @@
+package student_igors.lesson_6.level_1;
+
+public class NumberUtilsTest {
+
+}
+
+
+//   Напишите тесты для проверки метода isEven(int number).
+//      Упростите работу метода, код внутри может быть написан в одну строку.
