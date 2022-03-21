69833ce784f909f50a33372f98203f5c40f8fa22
student_igors <kork2@inbox.lv>
2022-03-21 07:56:09 +0200

    NumberUtilsTest

--- src/student_igors/lesson_6/level_1/NumberUtilsTest.java
+++ src/student_igors/lesson_6/level_1/NumberUtilsTest.java
@@ -1,8 +1,28 @@
 package student_igors.lesson_6.level_1;
 
 public class NumberUtilsTest {
+    public static void main(String[] args) {
+        NumberUtilsTest test = new NumberUtilsTest();
+        test.testIsEven();
+    }
 
+    public void testIsEven() {
+        int even = 22;
+        NumberUtils subject = new NumberUtils();
+        boolean actualResult = subject.isEven(even);
+        checkResult(actualResult, "If even test");
+    }
+
+    void checkResult(boolean actualResult, String testName) {
+        if (actualResult) {
+            System.out.println(testName + " Passed");
+        } else {
+            System.out.println(testName + " Failed");
+        }
+    }
 }
+
+
 
 
 //   Напишите тесты для проверки метода isEven(int number).
