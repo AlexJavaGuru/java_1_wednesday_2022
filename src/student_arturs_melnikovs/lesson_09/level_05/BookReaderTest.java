75c16a35484a260ae138e2f680ff1bb55acc82f0
ArtursMel <melnikov270@gmail.com>
2022-03-21 22:12:31 +0200

    ArturM: Lesson_09_Level_06

--- src/student_arturs_melnikovs/lesson_09/level_05/BookReaderTest.java
+++ src/student_arturs_melnikovs/lesson_09/level_05/BookReaderTest.java
@@ -42,10 +42,14 @@ class BookReaderTest {
         bookReader.addBook(lordOfRings2);
         bookReader.markAsUnread(lordOfRings);
         bookReader.markAsUnread(lordOfRings2);
-        List<String> expectedResult = new ArrayList<>() {{add("Lord Of The Ring [Tolkien]"); add("Lord Of The Ring 2 [Tolkien]");}};
+        List<String> expectedResult = new ArrayList<>() {{
+            add("Lord Of The Ring [Tolkien]");
+            add("Lord Of The Ring 2 [Tolkien]");
+        }};
         List<String> actualResult = bookReader.getListOfAllUnreadBooks();
         checkResult(expectedResult, actualResult, "Test");
     }
+
     public void testGetAllUnreadBooks2() {
         BookReader bookReader = new BookReaderImpl();
         createTestLibrary(bookReader);
@@ -53,6 +57,7 @@ class BookReaderTest {
         List<String> actualResult = bookReader.getListOfAllUnreadBooks();
         checkResult(expectedResult, actualResult, "Test");
     }
+
     public void testGetAllReadBooks1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -63,10 +68,14 @@ class BookReaderTest {
         bookReader.addBook(lordOfRings2);
         bookReader.markAsRead(lordOfRings);
         bookReader.markAsRead(lordOfRings2);
-        List<String> expectedResult = new ArrayList<>() {{add("Lord Of The Ring [Tolkien]"); add("Lord Of The Ring 2 [Tolkien]");}};
+        List<String> expectedResult = new ArrayList<>() {{
+            add("Lord Of The Ring [Tolkien]");
+            add("Lord Of The Ring 2 [Tolkien]");
+        }};
         List<String> actualResult = bookReader.getListOfAllReadBooks();
         checkResult(expectedResult, actualResult, "Test");
     }
+
     public void testGetAllReadBooks2() {
         BookReader bookReader = new BookReaderImpl();
         createTestLibrary(bookReader);
@@ -74,6 +83,7 @@ class BookReaderTest {
         List<String> actualResult = bookReader.getListOfAllReadBooks();
         checkResult(expectedResult, actualResult, "Test");
     }
+
     public void testMarkAsUnread1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -86,6 +96,7 @@ class BookReaderTest {
         boolean actualResult = lordOfRings.isUnread();
         checkResult(actualResult);
     }
+
     public void testMarkAsUnread2() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -97,6 +108,7 @@ class BookReaderTest {
         boolean actualResult = lordOfRings.isUnread();
         checkResult(!actualResult);
     }
+
     public void testMarkAsRead1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -109,6 +121,7 @@ class BookReaderTest {
         boolean actualResult = lordOfRings.isRead();
         checkResult(actualResult);
     }
+
     public void testMarkAsRead2() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -120,6 +133,7 @@ class BookReaderTest {
         boolean actualResult = lordOfRings.isRead();
         checkResult(!actualResult);
     }
+
     public void testFindByTitle1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -128,10 +142,13 @@ class BookReaderTest {
         bookReader.addBook(harryPotter);
         bookReader.addBook(lordOfRings);
         bookReader.addBook(lordOfRings2);
-        List<Book> expectedResult = new ArrayList<>() {{add(lordOfRings2);}};
+        List<Book> expectedResult = new ArrayList<>() {{
+            add(lordOfRings2);
+        }};
         List<Book> actualResult = bookReader.findByTitle("Lord Of The Ring 2");
         checkResult(expectedResult, actualResult);
     }
+
     public void testFindByTitle2() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -140,10 +157,14 @@ class BookReaderTest {
         bookReader.addBook(harryPotter);
         bookReader.addBook(lordOfRings);
         bookReader.addBook(lordOfRings2);
-        List<Book> expectedResult = new ArrayList<>() {{add(lordOfRings); add(lordOfRings2);}};
+        List<Book> expectedResult = new ArrayList<>() {{
+            add(lordOfRings);
+            add(lordOfRings2);
+        }};
         List<Book> actualResult = bookReader.findByTitle("Lord");
         checkResult(expectedResult, actualResult);
     }
+
     public void testFindByTitle3() {
         BookReader bookReader = new BookReaderImpl();
         createTestLibrary(bookReader);
@@ -151,6 +172,7 @@ class BookReaderTest {
         List<Book> actualResult = bookReader.findByTitle("Of The Ring");
         checkResult(expectedResult, actualResult);
     }
+
     public void testFindByAuthor() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -159,10 +181,14 @@ class BookReaderTest {
         bookReader.addBook(harryPotter);
         bookReader.addBook(lordOfRings);
         bookReader.addBook(lordOfRings2);
-        List<Book> expectedResult = new ArrayList<>() {{add(lordOfRings); add(lordOfRings2);}};
+        List<Book> expectedResult = new ArrayList<>() {{
+            add(lordOfRings);
+            add(lordOfRings2);
+        }};
         List<Book> actualResult = bookReader.findByAuthor("Tolkien");
         checkResult(expectedResult, actualResult);
     }
+
     public void testFindByAuthor2() {
         BookReader bookReader = new BookReaderImpl();
         createTestLibrary(bookReader);
@@ -170,6 +196,7 @@ class BookReaderTest {
         List<Book> actualResult = bookReader.findByAuthor("olkien");
         checkResult(expectedResult, actualResult);
     }
+
     public void testFindByAuthor3() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -178,10 +205,14 @@ class BookReaderTest {
         bookReader.addBook(harryPotter);
         bookReader.addBook(lordOfRings);
         bookReader.addBook(lordOfRings2);
-        List<Book> expectedResult = new ArrayList<>() {{add(lordOfRings); add(lordOfRings2);}};
+        List<Book> expectedResult = new ArrayList<>() {{
+            add(lordOfRings);
+            add(lordOfRings2);
+        }};
         List<Book> actualResult = bookReader.findByAuthor("Tolk");
         checkResult(expectedResult, actualResult);
     }
+
     public void testGetListOfAllBooks() {
         BookReader bookReader = new BookReaderImpl();
         createTestLibrary(bookReader);
@@ -189,6 +220,7 @@ class BookReaderTest {
         String[] actualResult = bookReader.getListOfAllBooks();
         checkResult(expectedResult, actualResult);
     }
+
     public void testDeleteBook1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -196,18 +228,21 @@ class BookReaderTest {
         boolean actualResult = bookReader.deleteBook(harryPotter);
         checkResult(actualResult);
     }
+
     public void testDeleteBook2() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
         boolean actualResult = bookReader.deleteBook(harryPotter);
         checkResult(!actualResult);
     }
+
     public void testAddBook1() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
         boolean actualResult = bookReader.addBook(harryPotter);
         checkResult(actualResult);
     }
+
     public void testAddBook2() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book("Harry Potter", "Rowling");
@@ -215,12 +250,14 @@ class BookReaderTest {
         boolean actualResult = bookReader.addBook(harryPotter);
         checkResult(!actualResult);
     }
+
     public void testAddBook3() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book();
         boolean actualResult = bookReader.addBook(harryPotter);
         checkResult(!actualResult);
     }
+
     public void testAddBook4() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book();
@@ -228,6 +265,7 @@ class BookReaderTest {
         boolean actualResult = bookReader.addBook(harryPotter);
         checkResult(!actualResult);
     }
+
     public void testAddBook5() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book();
@@ -235,6 +273,7 @@ class BookReaderTest {
         boolean actualResult = bookReader.addBook(harryPotter);
         checkResult(!actualResult);
     }
+
     public void testAddBook6() {
         BookReader bookReader = new BookReaderImpl();
         Book harryPotter = new Book();
@@ -251,6 +290,7 @@ class BookReaderTest {
             System.out.println("Test has failed");
         }
     }
+
     private void checkResult(String[] expectedResult, String[] actualResult) {
         if (Arrays.equals(expectedResult, actualResult)) {
             System.out.println("Test has passed");
@@ -258,6 +298,7 @@ class BookReaderTest {
             System.out.println("Test has failed");
         }
     }
+
     private void checkResult(List<Book> expectedResult, List<Book> actualResult) {
         if (expectedResult.equals(actualResult)) {
             System.out.println("Test has passed");
@@ -265,6 +306,7 @@ class BookReaderTest {
             System.out.println("Test has failed");
         }
     }
+
     private void checkResult(List<String> expectedResult, List<String> actualResult, String testName) {
         if (expectedResult.equals(actualResult)) {
             System.out.println("Test has passed");
@@ -272,6 +314,7 @@ class BookReaderTest {
             System.out.println("Test has failed");
         }
     }
+
     private void createTestLibrary(BookReader bookReader) {
         Book harryPotter = new Book("Harry Potter", "Rowling");
         Book lordOfRings = new Book("Lord Of The Ring", "Tolkien");
