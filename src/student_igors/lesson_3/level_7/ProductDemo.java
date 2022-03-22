710843f875c371b089ad7cbf2802b6a921b4074b
student_igors <kork2@inbox.lv>
2022-03-22 22:45:03 +0200

    ProductDemo

--- src/student_igors/lesson_3/level_7/ProductDemo.java
+++ src/student_igors/lesson_3/level_7/ProductDemo.java
@@ -3,8 +3,8 @@ package student_igors.lesson_3.level_7;
 public class ProductDemo {
     public static void main(String[] args) {
         Product product = new Product("Orange", 1.2, 10.0);
-        
-       Product orange = new Product("orange");
+
+        Product orange = new Product();
         orange.setRegularPrice(1.20);
         orange.setDiscount(0.12);
         orange.printInformation();
