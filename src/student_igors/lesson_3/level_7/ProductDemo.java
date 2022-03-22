0ad2c362ecce075910ab5efb0d6172cd3fd45c86
Trigs70 <98355804+Trigs70@users.noreply.github.com>
2022-03-22 22:26:22 +0200

    Update ProductDemo.java

--- src/student_igors/lesson_3/level_7/ProductDemo.java
+++ src/student_igors/lesson_3/level_7/ProductDemo.java
@@ -3,15 +3,12 @@ package student_igors.lesson_3.level_7;
 public class ProductDemo {
     public static void main(String[] args) {
         Product product = new Product("Orange", 1.2, 10.0);
-
-        double regularPrice = 1.2;
-        double discount = (1.2 * 10) / 100;
-        double result = (regularPrice - discount);
-
-         double actualPrice = result;
-
-
-        System.out.println("Result actual price is: " + actualPrice + " EUR");
+        
+       Product orange = new Product("orange");
+        orange.setRegularPrice(1.20);
+        orange.setDiscount(0.12);
+        orange.printInformation();
+  
 
     }
 }
