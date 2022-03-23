430199371b10c51662f740b4fa58d5b6a6f8c916
Jekaterina <kate.soldatova93@gmail.com>
2022-03-23 22:00:03 +0200

    BookReader task13

---
+++ src/student_jekaterina_soldatova/lesson_09/level_5_middle/BookReader.java
@@ -0,0 +1,5 @@
+package student_jekaterina_soldatova.lesson_09.level_5_middle;
+
+interface BookReader {
+
+}
---
+++ src/student_jekaterina_soldatova/lesson_09/level_5_middle/BookReaderImpl.java
@@ -0,0 +1,4 @@
+package student_jekaterina_soldatova.lesson_09.level_5_middle;
+
+class BookReaderImpl implements BookReader {
+}
