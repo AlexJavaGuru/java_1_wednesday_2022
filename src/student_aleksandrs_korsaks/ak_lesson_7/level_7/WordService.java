2a6237aa9e824c830e678a0a1727940ef84f113b
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-03-25 18:14:33 +0200

    Aleksandrs K.: lesson 7, level 7, Task 13 Refactored

--- src/student_aleksandrs_korsaks/ak_lesson_7/level_7/WordService.java
+++ src/student_aleksandrs_korsaks/ak_lesson_7/level_7/WordService.java
@@ -3,30 +3,44 @@ package student_aleksandrs_korsaks.ak_lesson_7.level_7;
 class WordService {
 
     public String findMostFrequentWord(String text) {
-        String[] textArray;
-        int[] wordCountArray;
-        textArray = text.split(" ");
-        wordCountArray = new int[textArray.length];
+
+        String[] wordsArray = createArrayOfWordsFromText(text);
+
+        int[] wordsRepetitionsArray = countWordRepetitions(wordsArray);
+
+        return findMaxRepeatedWord(wordsArray, wordsRepetitionsArray);
+    }
+
+    private String[] createArrayOfWordsFromText(String text) {
+        String[] wordsArray;
+        wordsArray = text.split(" ");
+        return wordsArray;
+    }
+
+    private int[] countWordRepetitions(String[] wordsArray) {
+        int[] wordsRepetitionsArray;
+        wordsRepetitionsArray = new int[wordsArray.length];
 
         int wordCounter = 0;
-        for (int wordCount = 0; wordCount < textArray.length; wordCount++) {
-            for (String s : textArray) {
-                if (textArray[wordCount].equals(s)) {
+        for (int wordCount = 0; wordCount < wordsArray.length; wordCount++) {
+            for (String word : wordsArray) {
+                if (wordsArray[wordCount].equals(word)) {
                     wordCounter++;
                 }
             }
-            wordCountArray[wordCount] = wordCounter;
+            wordsRepetitionsArray[wordCount] = wordCounter;
             wordCounter = 0;
         }
-
-        int max = 0;
-        for (int count = 0; count < wordCountArray.length; count++) {
-            if (wordCountArray[count] > wordCountArray[max]) {
-                max = count;
-
-            }
-        }
-        return textArray[max];
+        return wordsRepetitionsArray;
     }
 
+    private String findMaxRepeatedWord(String[] wordsArray, int[] wordsRepetitionsArray) {
+        int max = 0;
+        for (int count = 0; count < wordsRepetitionsArray.length; count++) {
+            if (wordsRepetitionsArray[count] > wordsRepetitionsArray[max]) {
+                max = count;
+            }
+        }
+        return wordsArray[max];
+    }
 }
