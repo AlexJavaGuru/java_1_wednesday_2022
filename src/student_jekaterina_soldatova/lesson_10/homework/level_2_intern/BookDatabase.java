f08efd8aa7a79a0d73be8cb2ef5fb89a476c0ba6
Jekaterina <kate.soldatova93@gmail.com>
2022-04-01 21:45:31 +0300

    task12 book database

--- src/student_jekaterina_soldatova/lesson_10/homework/level_2_intern/BookDatabase.java
+++ src/student_jekaterina_soldatova/lesson_10/homework/level_2_intern/BookDatabase.java
@@ -1,10 +1,23 @@
 package student_jekaterina_soldatova.lesson_10.homework.level_2_intern;
 
+import java.util.List;
+import java.util.Optional;
+
 interface BookDatabase {
 
     Long save(Book book);
 
     boolean delete(Long bookId);
+
+    boolean delete(Book book);
+
+    Optional<Book> findById(Long bookId);
+
+    List<Book> findByAuthor(String author);
+
+    List<Book> findByTitle(String title);
+
+    int countAllBooks();
 
 
 }
--- src/student_jekaterina_soldatova/lesson_10/homework/level_2_intern/BookDatabaseImpl.java
+++ src/student_jekaterina_soldatova/lesson_10/homework/level_2_intern/BookDatabaseImpl.java
@@ -2,6 +2,7 @@ package student_jekaterina_soldatova.lesson_10.homework.level_2_intern;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 class BookDatabaseImpl implements BookDatabase{
 
@@ -22,6 +23,52 @@ class BookDatabaseImpl implements BookDatabase{
             }
         }
         return false;
+    }
+
+    @Override
+    public boolean delete(Book book) {
+        if (bookList.contains(book)) {
+            bookList.remove(book);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Optional<Book> findById(Long bookId) {
+        for (Book book: bookList) {
+            if (book.getId() == bookId) {
+                Optional.of(book);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findByAuthor(String author) {
+        List<Book> listOfBooksByAuthor = new LinkedList<>();
+        for (Book book: bookList) {
+            if (book.getAuthor().equals(author)) {
+                listOfBooksByAuthor.add(book);
+            }
+        }
+        return listOfBooksByAuthor;
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        List<Book> listOfBooksWithTitle = new LinkedList<>();
+        for (Book book: bookList) {
+            if (book.getTitle().equals(title)) {
+                listOfBooksWithTitle.add(book);
+            }
+        }
+        return listOfBooksWithTitle;
+    }
+
+    @Override
+    public int countAllBooks() {
+        return bookList.size();
     }
 
 }
