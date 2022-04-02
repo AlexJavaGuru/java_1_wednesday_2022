7bfabdfa3c5523664b56df27f89e560cfefe04f0
student_igors <kork2@inbox.lv>
2022-04-02 03:34:53 +0300

    DayOfTheWeekDetector

---
+++ src/student_igors/lesson_6/level_2/DayOfTheWeekDetector.java
@@ -0,0 +1,27 @@
+package student_igors.lesson_6.level_2;
+
+import java.util.Scanner;
+
+public class DayOfTheWeekDetector {
+
+    public int getDayNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input day number between 1 and 7 : ");
+        return scanner.nextInt();
+    }
+
+    public String findDayOfTheWeek(int dayNumber) {
+        String result;
+        switch (dayNumber) {
+            case 1 -> result = "Monday";
+            case 2 -> result = "Tuesday";
+            case 3 -> result = "Wednesday";
+            case 4 -> result = "Thursday";
+            case 5 -> result = "Friday";
+            case 6 -> result = "Saturday";
+            case 7 -> result = "Sunday";
+            default -> result = "Not correct day number";
+        }
+        return result;
+    }
+}
