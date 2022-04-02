4e4f8653771d4f418a4efdf2d176bc25a015e30f
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-02 20:31:02 +0300

    Aleksandrs K.: lesson 8, level 4, Task 15

---
+++ src/student_aleksandrs_korsaks/ak_lesson_8/level_4/Shape.java
@@ -0,0 +1,15 @@
+package student_aleksandrs_korsaks.ak_lesson_8.level_4;
+
+abstract class Shape {
+
+    private String title;
+
+    Shape(String title) {
+        this.title = title;
+    }
+
+    abstract double calculateArea();
+
+    abstract double calculatePerimeter();
+
+}
