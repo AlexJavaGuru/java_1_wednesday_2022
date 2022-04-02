9262561487883be1cec1292c89ed31943b07ec59
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-02 21:48:48 +0300

    Aleksandrs K.: lesson 8, level 4, Task 16

---
+++ src/student_aleksandrs_korsaks/ak_lesson_8/level_4/Circle.java
@@ -0,0 +1,22 @@
+package student_aleksandrs_korsaks.ak_lesson_8.level_4;
+
+class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(String title, double radius) {
+        super(title);
+        this.radius = radius;
+    }
+
+
+    @Override
+    double calculateArea() {
+        return Math.round((Math.PI * Math.pow(radius, 2)) * 100.00) / 100.00;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return Math.round((2 * Math.PI * radius) * 100.00) / 100.00;
+    }
+}
