1a15c57a9c6e6036961dadcedb1231975d71f916
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-02 16:51:40 +0300

    Aleksandrs K.: lesson 8, level 1, Task 2

---
+++ src/student_aleksandrs_korsaks/ak_lesson_8/level_1/DefaultConstructorV2Demo.java
@@ -0,0 +1,7 @@
+package student_aleksandrs_korsaks.ak_lesson_8.level_1;
+
+public class DefaultConstructorV2Demo {
+    public static void main(String[] args) {
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+}
