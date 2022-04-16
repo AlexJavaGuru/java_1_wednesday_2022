57302871264dee86be96638cf03d36cd8a5edbb3
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-16 20:12:34 +0300

    Aleksandrs K.: lesson 9, level 1, Task 2

---
+++ src/student_aleksandrs_korsaks/ak_lesson_9/level_1/ArrayUtil.java
@@ -0,0 +1,15 @@
+package student_aleksandrs_korsaks.ak_lesson_9.level_1;
+
+interface ArrayUtil {
+
+    int[] createArray(int arrayLength);
+
+    void fillArrayWithRandomNumbers(int[] array);
+
+    void printArrayToConsole(int[] array);
+
+    int findMaxNumber(int[] array);
+
+    int findMinNumber(int[] array);
+
+}
