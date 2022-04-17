1d04b2e5b14c97f78a54415c171767413440a214
JaKo <jako2577@gmail.com>
2022-04-17 12:53:48 +0300

    Level 2

---
+++ src/student_aleksandrs_jakovenko/lesson_12/level_2/BankApiImpl.java
@@ -0,0 +1,28 @@
+package student_aleksandrs_jakovenko.lesson_12.level_2;
+
+import java.util.List;
+import java.util.Optional;
+
+class BankApiImpl implements BankApi {
+
+    private List<BankClient> clients;
+
+    public BankApiImpl(List<BankClient> clients) {
+        this.clients = clients;
+    }
+
+    public Optional<BankClient> findByUid(UserCredentials credentials,
+                                          String uid) throws AccessDeniedException {
+        if (!credentials.hasRole(Role.CAN_SEARCH_CLIENTS)) {
+            throw new AccessDeniedException();
+        }
+
+        for (BankClient client : clients) {
+            if (client.getUid().equals(uid)) {
+                return Optional.of(client);
+            }
+        }
+        return Optional.empty();
+    }
+
+}
---
+++ src/student_aleksandrs_jakovenko/lesson_12/level_2/UserCredentials.java
@@ -0,0 +1,20 @@
+package student_aleksandrs_jakovenko.lesson_12.level_2;
+
+import java.util.List;
+
+class UserCredentials {
+
+    private List<Role> roles;
+
+    public UserCredentials(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public boolean hasRole(Role role) {
+        return roles.contains(role);
+    }
+}
