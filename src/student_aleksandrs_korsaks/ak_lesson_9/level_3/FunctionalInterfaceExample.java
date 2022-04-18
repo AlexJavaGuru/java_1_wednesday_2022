4085d2b2a501898228ea9b66bb938766074a0580
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-18 18:59:10 +0300

    Aleksandrs K.: lesson 9, level 3, Task 6

---
+++ src/student_aleksandrs_korsaks/ak_lesson_9/level_3/FunctionalInterfaceExample.java
@@ -0,0 +1,17 @@
+package student_aleksandrs_korsaks.ak_lesson_9.level_3;
+
+@FunctionalInterface
+interface FunctionalInterfaceExample {
+
+    boolean isOddNumber (int number);
+
+ /*
+ Функциональный интерфейс — это интерфейс,
+ который содержит ровно один абстрактный метод,
+ то есть описание метода без тела.
+ Статические методы и методы по умолчанию при этом не в счёт,
+ их в функциональном интерфейсе может быть сколько угодно.
+  */
+
+
+}
