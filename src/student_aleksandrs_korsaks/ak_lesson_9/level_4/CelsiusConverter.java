4a32489a7056c1e36c9d074e3b4115b93614f972
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-04-19 20:09:11 +0300

    Aleksandrs K.: lesson 9, level 4, Task 11-12

---
+++ src/student_aleksandrs_korsaks/ak_lesson_9/level_4/CelsiusConverter.java
@@ -0,0 +1,9 @@
+package student_aleksandrs_korsaks.ak_lesson_9.level_4;
+
+class CelsiusConverter implements TemperatureConverter {
+
+    @Override
+    public double convert(double celsiusTemperature) {
+        return celsiusTemperature;
+    }
+}
