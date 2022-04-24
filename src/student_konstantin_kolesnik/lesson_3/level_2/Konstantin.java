947d36c02bbd5b67620a85c2965e9ec8f180d7db
AlexJavaGuru <55928462+AlexJavaGuru@users.noreply.github.com>
2022-04-24 12:49:23 +0300

    Teacher Commit to fix the mess

--- src/student_konstantin_kolesnik/lesson_3/level_2/Konstantin.java
+++
@@ -1,9 +0,0 @@
-package student_konstantin_kolesnik.lesson_3.level_2;
-
-public class Konstantin {
-    public static void main(String[] args) {
-
-        String myName = "Konstantin";
-        System.out.println(myName);
-    }
-}
--- src/student_konstantin_kolesnik/lesson_3/level_7/ProductDemo.java
+++
@@ -1,9 +0,0 @@
-package student_konstantin_kolesnik.lesson_3.level_7;
-
-public class ProductDemo {
-
-    public static void main(String[] args) {
-    Product milk = new Product("Milk", 1.49, 3.4);
-    milk.print();
-    }
-}
