9f3a22429bcda98575f8626a2913cf94db6d80fc
Jekaterina <kate.soldatova93@gmail.com>
2022-04-24 19:02:04 +0300

    lvl 1

--- src/main/java/student_jekaterina_soldatova/lesson11/level_1_intern/SafeInteractableDemo.java
+++ src/main/java/student_jekaterina_soldatova/lesson11/level_1_intern/SafeInteractableDemo.java
@@ -4,8 +4,8 @@ class SafeInteractableDemo {
 
     public static void main(String[] args) {
         SafeInteractable safe = new SafeInteractable("012368f", 56.87);
-        safe.putMoneyInSafe("password", 965.09);
-        System.out.println(safe.getMoneyFromSafe("password", 45.90));
+        safe.putMoneyInSafe("012368f", 965.09);
+        System.out.println(safe.getMoneyFromSafe("012368f", 45.90));
     }
 }
 
