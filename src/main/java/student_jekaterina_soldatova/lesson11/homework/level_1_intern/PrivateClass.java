454e531b43cdd0c2eba6aba1519dce30be01d69f
Jekaterina <kate.soldatova93@gmail.com>
2022-04-24 17:53:47 +0300

    lvl 1

---
+++ src/main/java/student_jekaterina_soldatova/lesson11/homework/level_1_intern/PrivateClass.java
@@ -0,0 +1,5 @@
+package student_jekaterina_soldatova.lesson11.homework.level_1_intern;
+
+class PrivateClass {
+    private int variable;
+}
---
+++ src/main/java/student_jekaterina_soldatova/lesson11/homework/level_1_intern/Rocket.java
@@ -0,0 +1,4 @@
+package student_jekaterina_soldatova.lesson11.homework.level_1_intern;
+
+class Rocket {
+}
