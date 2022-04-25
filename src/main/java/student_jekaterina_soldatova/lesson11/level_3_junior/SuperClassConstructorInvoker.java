cdd44e86731f1d2aaccae14bc8dd61dd32af38b7
Jekaterina <kate.soldatova93@gmail.com>
2022-04-25 15:58:32 +0300

    lvl 2

---
+++ src/main/java/student_jekaterina_soldatova/lesson11/level_3_junior/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_jekaterina_soldatova.lesson11.level_3_junior;
+
+public class SuperClassConstructorInvoker{
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
