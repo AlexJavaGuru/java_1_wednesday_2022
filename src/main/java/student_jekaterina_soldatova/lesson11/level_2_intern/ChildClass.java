1bc646f0da63d5a8389eb6150881cddb8e157be6
Jekaterina <kate.soldatova93@gmail.com>
2022-04-25 15:52:42 +0300

    lvl 2

---
+++ src/main/java/student_jekaterina_soldatova/lesson11/level_2_intern/ChildClass.java
@@ -0,0 +1,8 @@
+package student_jekaterina_soldatova.lesson11.level_2_intern;
+
+class ChildClass extends ParentClass {
+
+    ChildClass(int parameterCount) {
+        super(parameterCount);
+    }
+}
---
+++ src/main/java/student_jekaterina_soldatova/lesson11/level_2_intern/ParentClass.java
@@ -0,0 +1,10 @@
+package student_jekaterina_soldatova.lesson11.level_2_intern;
+
+class ParentClass {
+
+    int parameterCount;
+
+    protected ParentClass(int parameterCount) {
+        this.parameterCount = parameterCount;
+    }
+}
