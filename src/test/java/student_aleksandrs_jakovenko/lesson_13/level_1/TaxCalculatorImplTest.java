fa14b721e610fd17ada4198edff0994e374c1aa9
JaKo <jako2577@gmail.com>
2022-05-04 12:37:13 +0300

    Level 1

---
+++ src/test/java/student_aleksandrs_jakovenko/lesson_13/level_1/TaxCalculatorImplTest.java
@@ -0,0 +1,40 @@
+package student_aleksandrs_jakovenko.lesson_13.level_1;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaxCalculatorImplTest {
+
+    private TaxCalculator taxCalculator = new TaxCalculatorImpl();
+
+    @Test
+    public void shouldPayZeroTaxWhenIncomeIsZero() {
+        var income = BigDecimal.ZERO;
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("0.00"));
+    }
+
+    @Test
+    public void shouldPay25PercentTaxWhenIncomeLessThen20000() {
+        var income = new BigDecimal(10000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("2500.00"));
+    }
+
+    @Test
+    public void shouldPay25PercentTaxWhenIncomeEqualTo20000() {
+        var income = new BigDecimal(20000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("5000.00"));
+    }
+
+    @Test
+    public void shouldPay40PercentTaxWhenIncomeBiggerThen20000() {
+        var income = new BigDecimal(30000);
+        var taxToPay = taxCalculator.calculateTax(income);
+        assertEquals(taxToPay, new BigDecimal("9000.00"));
+    }
+}
