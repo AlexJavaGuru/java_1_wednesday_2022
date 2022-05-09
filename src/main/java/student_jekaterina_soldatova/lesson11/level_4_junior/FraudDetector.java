e6ab8483a1fe86c1c9b465c1bf669d9c63650090
Jekaterina <kate.soldatova93@gmail.com>
2022-05-09 22:21:29 +0300

    lvl 5

--- src/main/java/student_jekaterina_soldatova/lesson11/level_4_junior/FraudDetector.java
+++ src/main/java/student_jekaterina_soldatova/lesson11/level_4_junior/FraudDetector.java
@@ -1,36 +1,22 @@
 package student_jekaterina_soldatova.lesson11.level_4_junior;
 
+import java.util.List;
+
 class FraudDetector {
 
+    List<FraudRule> fraudRules;
+
+    public FraudDetector(List<FraudRule> fraudRules) {
+        this.fraudRules = fraudRules;
+    }
+
     boolean isFraud(Transaction t) {
-        return (traderNameCheck(t) || amountTooBigCheck(t) || traderCityCheck(t) || traderCountryCheck(t) || traderGermanAmount(t));
-    }
-
-    boolean traderNameCheck(Transaction t) {
-        return t.getTrader().getFullName().equals("Pokemon");
-    }
-
-    boolean amountTooBigCheck(Transaction t) {
-        return t.getAmount()>1000000;
-    }
-
-    boolean traderCityCheck(Transaction t) {
-        return (t.getTrader().getCity().equals("Sydney"));
-    }
-
-    boolean traderCountryCheck(Transaction t) {
-        if (t.getTrader().getCountry() == null) {
-            return false;
-        } else {
-            return t.getTrader().getCountry().equals("Jamaica");
+        for (FraudRule rule : fraudRules) {
+            if (rule.isFraud(t)) {
+                return true;
+            }
         }
+        return false;
     }
 
-    boolean traderGermanAmount(Transaction t) {
-        if (t.getTrader().getCountry() == null) {
-            return false;
-        } else {
-            return (t.getTrader().getCountry().equals("Germany") && t.getAmount() > 1000);
-        }
-    }
 }
