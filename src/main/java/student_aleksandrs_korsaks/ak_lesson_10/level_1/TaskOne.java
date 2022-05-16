644690941598f1111b21cb2c20e937a007b2da54
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-05-16 18:27:06 +0300

    Aleksandrs K.: lesson 10, level 1, Task 2

--- src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_1/TaskOne.java
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_1/TaskOne.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class TaskOne {
+class TaskOne {
     public static void main(String[] args) {
 
         List<String> demoArrayList = new ArrayList<>();
