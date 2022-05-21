5230dab2c28b5a82499bd3358d9ea4b2db93208b
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-05-21 23:43:24 +0300

    Aleksandrs K.: Lesson 10, Level 3, Task 19

---
+++ src/test/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3/AndSearchCriteriaTest.java
@@ -0,0 +1,23 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AndSearchCriteriaTest {
+
+    AndSearchCriteria andSearchCriteria = new AndSearchCriteria(new AuthorSearchCriteria("A1"),
+            new YearOfIssueSearchCriteria("1990"));
+
+    @Test
+    void matchPositive() {
+        Book bookOne = new Book("A1", "B1", "1990");
+        assertTrue(andSearchCriteria.match(bookOne));
+    }
+
+    @Test
+    void matchNegative() {
+        Book bookTwo = new Book("A1", "B1", "1991");
+        assertFalse(andSearchCriteria.match(bookTwo));
+    }
+}
