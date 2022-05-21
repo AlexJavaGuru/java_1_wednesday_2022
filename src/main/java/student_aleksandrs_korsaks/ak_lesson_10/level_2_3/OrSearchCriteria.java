fc365e7957a4210f2c3509efa22ab4e4dfb7b7d1
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-05-21 23:54:50 +0300

    Aleksandrs K.: Lesson 10, Level 3, Task 20

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3/OrSearchCriteria.java
@@ -0,0 +1,18 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3;
+
+class OrSearchCriteria implements SearchCriteria {
+
+    private SearchCriteria leftCondition;
+    private SearchCriteria rightCondition;
+
+    public OrSearchCriteria(SearchCriteria leftCondition,
+                            SearchCriteria rightCondition) {
+        this.leftCondition = leftCondition;
+        this.rightCondition = rightCondition;
+    }
+
+    public boolean match(Book book) {
+        return leftCondition.match(book) || rightCondition.match(book);
+    }
+
+}
---
+++ src/test/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3/OrSearchCriteriaTest.java
@@ -0,0 +1,29 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrSearchCriteriaTest {
+
+    OrSearchCriteria orSearchCriteria = new OrSearchCriteria(new AuthorSearchCriteria("A1"),
+            new TitleSearchCriteria("B1"));
+
+    @Test
+    void matchPositiveOne() {
+        Book bookOne = new Book("A1", "B2", "1990");
+        assertTrue(orSearchCriteria.match(bookOne));
+    }
+
+    @Test
+    void matchPositiveTwo() {
+        Book bookTwo = new Book("A1", "B1", "1990");
+        assertTrue(orSearchCriteria.match(bookTwo));
+    }
+
+    @Test
+    void matchNegative() {
+        Book bookThree = new Book("A2", "B2", "1990");
+        assertFalse(orSearchCriteria.match(bookThree));
+    }
+}
