0d1612ecb836d76030f400846ac214f1096fdc29
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-05-21 18:03:33 +0300

    Aleksandrs K.: Lesson 10, Level 3, Task 17

---
+++ src/test/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3/TitleSearchCriteriaTest.java
@@ -0,0 +1,22 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TitleSearchCriteriaTest {
+
+    TitleSearchCriteria titleSearchCriteria = new TitleSearchCriteria("B1");
+
+    @Test
+    void matchPositive() {
+        Book bookOne = new Book("A1", "B1");
+        assertTrue(titleSearchCriteria.match(bookOne));
+    }
+
+    @Test
+    void matchNegative() {
+        Book bookTwo = new Book("A2", "B2");
+        assertFalse(titleSearchCriteria.match(bookTwo));
+    }
+}
