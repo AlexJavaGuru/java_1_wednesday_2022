f7c07899c8dc10a009a739ea6e7bbd14ce34d222
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-02 23:17:34 +0300

    Aleksandrs K.: Lesson 10, Level 4, Task 27

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3_4/UniqueWordFinder.java
@@ -0,0 +1,14 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3_4;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class UniqueWordFinder {
+
+    Set<String> find(String text) {
+        return new HashSet<>(List.of(text.toLowerCase()
+                .replaceAll("[^a-zA-Z\s]", " ")
+                .split("\s+")));
+    }
+}
