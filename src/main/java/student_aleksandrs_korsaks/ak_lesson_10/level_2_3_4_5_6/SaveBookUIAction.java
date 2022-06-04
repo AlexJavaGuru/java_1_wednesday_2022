0c28980c651c54e3262a76541951feae85ebecd3
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-04 21:22:50 +0300

    Aleksandrs K.: lesson 10, level 6, Task 31

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3_4_5_6/SaveBookUIAction.java
@@ -0,0 +1,27 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3_4_5_6;
+
+import java.util.Scanner;
+
+class SaveBookUIAction implements UIAction {
+
+    private BookDatabase bookDatabase;
+
+    public SaveBookUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    public void execute() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the book`s author");
+        String authorName = scanner.nextLine();
+        System.out.println("Please enter the book`s title");
+        String bookTitle = scanner.nextLine();
+        System.out.println("Please enter the book`s year of production");
+        String bookYearOfProduction = scanner.nextLine();
+        scanner.close();
+
+        Book newBook = new Book(authorName, bookTitle, bookYearOfProduction);
+        bookDatabase.save(newBook);
+    }
+}
