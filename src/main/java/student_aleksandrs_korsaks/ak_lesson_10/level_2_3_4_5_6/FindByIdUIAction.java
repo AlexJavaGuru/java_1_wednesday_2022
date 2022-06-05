869ddd74600bb8e3f0f4201adc56988c03a90562
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-05 17:22:35 +0300

    Aleksandrs K.: lesson 10, level 6, Task 32

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_10/level_2_3_4_5_6/FindByIdUIAction.java
@@ -0,0 +1,29 @@
+package student_aleksandrs_korsaks.ak_lesson_10.level_2_3_4_5_6;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+class FindByIdUIAction implements UIAction {
+
+    private BookDatabase bookDatabase;
+
+    public FindByIdUIAction(BookDatabase bookDatabase) {
+        this.bookDatabase = bookDatabase;
+    }
+
+    public void execute() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the book`s ID");
+        Long bookId = scanner.nextLong();
+        scanner.close();
+
+        Optional<Book> bookOpt = bookDatabase.findById(bookId);
+
+        if (bookOpt.isPresent()) {
+            System.out.println("Result for book with ID " + bookId + " is: " + bookOpt);
+        } else {
+            System.out.println("there is no book with ID - " + bookId);
+        }
+    }
+}
