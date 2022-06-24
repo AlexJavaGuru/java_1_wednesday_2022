06b2d761edc06140e7967b235dbd4ecee84cb366
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-24 16:23:01 +0300

    Aleksandrs K.: Lesson 11, level 1, Task 7

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_1/PrivateClass.java
@@ -0,0 +1,11 @@
+package student_aleksandrs_korsaks.ak_lesson_11.level_1;
+
+class PrivateClass {
+
+    private String title;
+    private String author;
+
+    public static void main(String[] args) {
+
+    }
+}
