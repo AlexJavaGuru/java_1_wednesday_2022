e52b3de55eea84c59b316b5af9ba30abbcdc0ee2
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-24 23:39:47 +0300

    Aleksandrs K.: Lesson 11, level 2, Task 13

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_2/ClassX.java
@@ -0,0 +1,7 @@
+package student_aleksandrs_korsaks.ak_lesson_11.level_2;
+
+class ClassX {
+    public ClassX(int i) {
+        System.out.println(1);
+    }
+}
---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_2/ClassY.java
@@ -0,0 +1,8 @@
+package student_aleksandrs_korsaks.ak_lesson_11.level_2;
+
+class ClassY extends ClassX {
+    public ClassY(int i) {
+        super(i);
+        System.out.println(2);
+    }
+}
