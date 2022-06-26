d112620422497f750e6de0cde8e8a97daba965c2
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-26 16:22:25 +0300

    Aleksandrs K.: Lesson 11, level 4, Task 18

--- src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetector.java
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetector.java
@@ -2,8 +2,11 @@ package student_aleksandrs_korsaks.ak_lesson_11.level_4;
 
 class FraudDetector {
 
+    public static final int TRX_AMOUNT_LIMIT = 1000000;
+
     boolean isFraud(Transaction t) {
-        return t.getTrader().getFullName().equals("Pokemon");
+        return t.getTrader().getFullName().equals("Pokemon") ||
+                (t.getAmount() > TRX_AMOUNT_LIMIT);
     }
 
 }
--- src/test/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetectorTest.java
+++ src/test/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetectorTest.java
@@ -4,22 +4,37 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static student_aleksandrs_korsaks.ak_lesson_11.level_4.FraudDetector.TRX_AMOUNT_LIMIT;
 
 class FraudDetectorTest {
 
     FraudDetector fraudDetector = new FraudDetector();
 
     @Test
-    void isFraudTrueCase() {
+    void isFraudNotValidTraderNameTrueCase() {
         Trader trader = new Trader("Pokemon", "Riga");
         Transaction transaction = new Transaction(trader, 100);
         assertTrue(fraudDetector.isFraud(transaction));
     }
 
     @Test
-    void isFraudFalseCase() {
+    void isFraudValidTraderNameFalseCase() {
         Trader trader = new Trader("John", "Riga");
         Transaction transaction = new Transaction(trader, 100);
+        assertFalse(fraudDetector.isFraud(transaction));
+    }
+
+    @Test
+    void isFraudNotValidTrxAmountTrueCase() {
+        Trader trader = new Trader("John", "Riga");
+        Transaction transaction = new Transaction(trader, TRX_AMOUNT_LIMIT + 1);
+        assertTrue(fraudDetector.isFraud(transaction));
+    }
+
+    @Test
+    void isFraudValidTrxAmountFalseCase() {
+        Trader trader = new Trader("John", "Riga");
+        Transaction transaction = new Transaction(trader, TRX_AMOUNT_LIMIT);
         assertFalse(fraudDetector.isFraud(transaction));
     }
 }
