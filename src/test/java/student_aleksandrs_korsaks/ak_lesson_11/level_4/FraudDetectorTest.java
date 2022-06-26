9bc2997dc87e68f960e37ad3a2352d94907c0ded
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-26 18:39:37 +0300

    Aleksandrs K.: Lesson 11, level 4, Task 21

--- src/test/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetectorTest.java
+++ src/test/java/student_aleksandrs_korsaks/ak_lesson_11/level_4/FraudDetectorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static student_aleksandrs_korsaks.ak_lesson_11.level_4.FraudDetector.TRX_AMOUNT_LIMIT;
+import static student_aleksandrs_korsaks.ak_lesson_11.level_4.FraudDetector.TRX_AMOUNT_LIMIT_GERMANY;
 
 class FraudDetectorTest {
 
@@ -63,6 +64,20 @@ class FraudDetectorTest {
     void isFraudValidTraderCountryFalseCase() {
         Trader trader = new Trader("John", "Riga", "Latvia");
         Transaction transaction = new Transaction(trader, TRX_AMOUNT_LIMIT);
+        assertFalse(fraudDetector.isFraud(transaction));
+    }
+
+    @Test
+    void isFraudNotValidTraderCountryAndTrxAmountTrueCase() {
+        Trader trader = new Trader("John", "Berlin", "Germany");
+        Transaction transaction = new Transaction(trader, TRX_AMOUNT_LIMIT_GERMANY + 1);
+        assertTrue(fraudDetector.isFraud(transaction));
+    }
+
+    @Test
+    void isFraudNotValidTraderCountryValidTrxAmountFalseCase() {
+        Trader trader = new Trader("John", "Berlin", "Germany");
+        Transaction transaction = new Transaction(trader, TRX_AMOUNT_LIMIT_GERMANY);
         assertFalse(fraudDetector.isFraud(transaction));
     }
 }
