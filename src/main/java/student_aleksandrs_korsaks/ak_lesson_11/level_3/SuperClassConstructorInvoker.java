26bec70d76f32606c8256e145d43e0f4abab2dd7
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-06-26 15:10:29 +0300

    Aleksandrs K.: Lesson 11, level 3, Task 16

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_11/level_3/SuperClassConstructorInvoker.java
@@ -0,0 +1,9 @@
+package student_aleksandrs_korsaks.ak_lesson_11.level_3;
+
+class SuperClassConstructorInvoker {
+    public SuperClassConstructorInvoker() {
+        super();
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
