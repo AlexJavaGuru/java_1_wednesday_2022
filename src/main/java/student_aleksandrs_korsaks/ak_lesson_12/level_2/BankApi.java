adedff34f0e3ddc129ce1ec372d5ad3aa8fb5578
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-07-08 20:30:53 +0300

    Aleksandrs K.: Lesson 12, level 2,Task 11

--- src/main/java/student_aleksandrs_korsaks/ak_lesson_12/level_2/BankApi.java
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_12/level_2/BankApi.java
@@ -4,5 +4,5 @@ import java.util.Optional;
 
 interface BankApi {
 
-    Optional<BankClient> findByUid(UserCredentials credentials, String uid) throws IllegalAccessException;
+    Optional<BankClient> findByUid(UserCredentials credentials, String uid) throws AccessDeniedException;
 }
