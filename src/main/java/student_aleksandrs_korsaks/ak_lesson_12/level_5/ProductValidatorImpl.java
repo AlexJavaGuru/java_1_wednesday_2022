ab0e184310f02588f48af28d23e33a89bcae8170
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-07-12 19:34:45 +0300

    Aleksandrs K.: Lesson 12, level 5,Task 34

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_12/level_5/ProductValidatorImpl.java
@@ -0,0 +1,12 @@
+package student_aleksandrs_korsaks.ak_lesson_12.level_5;
+
+import java.util.List;
+
+class ProductValidatorImpl implements ProductValidator {
+
+    @Override
+    public List<ValidationException> validate(Product product) {
+        return null;
+    }
+
+}
