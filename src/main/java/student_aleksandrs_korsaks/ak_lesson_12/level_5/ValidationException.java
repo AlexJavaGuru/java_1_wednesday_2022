342c5a6232cdd82dbbc4b05dc947610194d6d4d4
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-07-12 19:13:57 +0300

    Aleksandrs K.: Lesson 12, level 5,Task 30-31

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_12/level_5/ValidationException.java
@@ -0,0 +1,27 @@
+package student_aleksandrs_korsaks.ak_lesson_12.level_5;
+
+class ValidationException extends Exception{
+
+    private String ruleName;
+    private String description;
+    private String fieldName;
+
+    public ValidationException(String ruleName, String description, String fieldName) {
+        this.ruleName = ruleName;
+        this.description = description;
+        this.fieldName = fieldName;
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+}
