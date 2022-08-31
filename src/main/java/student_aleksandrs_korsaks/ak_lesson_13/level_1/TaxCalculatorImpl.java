cfc7007889265863b320961abaaa9cfbdeae4f91
Aleksandrs2016 <korsaks.aleksandrs@gmail.com>
2022-08-31 20:48:16 +0300

    Aleksandrs K.: lesson 13, level 1, Task 2

---
+++ src/main/java/student_aleksandrs_korsaks/ak_lesson_13/level_1/TaxCalculatorImpl.java
@@ -0,0 +1,10 @@
+package student_aleksandrs_korsaks.ak_lesson_13.level_1;
+
+import java.math.BigDecimal;
+
+class TaxCalculatorImpl implements TaxCalculator {
+    @Override
+    public BigDecimal calculateTax(BigDecimal income) {
+        return null;
+    }
+}
